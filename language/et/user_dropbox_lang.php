6232d5030bc84d3abafcfa0cf4ceda097d5bc25f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:41:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox on pilvepõhine failide hoidmiseks ja sünkroniseerimine teenust. Kasutage seda app sünkroonida faile kausta mis asub sinu kodukataloogis, mida saab seejärel kasutada mingi seade, mis on seotud sama Dropbox konto (sülearvuti, mobiiltelefoni, tablett jne).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Autentimine Dropbox';
+$lang['user_dropbox_authentication'] = 'Täielik setup autentimisel Dropbox';
+$lang['user_dropbox_create_account'] = 'Loo Dropbox konto';
+$lang['user_dropbox_have_account'] = 'Mul on juba Dropbox konto';
+$lang['user_dropbox_init_complete'] = 'Initsialiseerimisteenuseid täielik. Palun kliki lingil, et lõpetada Dropbox luba oma kontole.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Sa pead Dropbox konto sünkroonida kohaliku serveri kausta pilvepõhise teenusega.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Nüüd, kui olete oma Dropbox konto seadistamine, vajuta "Sünkrooni kohe" nuppu. Sul on varustatud URL sisaldab võti Dropbox kausta elavad oma kodus kataloogi. Kasutage seda linki siduda oma Dropbox konto sihtkohta klõpsates seda ja täites Dropbox luba.';
+$lang['user_dropbox_invalid_account'] = 'Konto on kehtetu.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Vabandame, Dropbox teenus ei ole kättesaadav root.';
+$lang['user_dropbox_starting_account_init'] = 'Alustades konto initsialiseerimise ... palun kannatlik.';
+$lang['user_dropbox_step_1'] = 'Etapp 1 2';
+$lang['user_dropbox_step_2'] = 'Etapp 2 2-';
+$lang['user_dropbox_sync_check'] = 'Saate Sync';
+$lang['user_dropbox_sync_now'] = 'Sünkrooni kohe';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Ootan Dropbox konto sünkroniseerimine autentimise lõpetada. Palun kliki Dropbox URL link allpool, kui sa ei ole käinud Dropbox saidi ja seotud kontol.';
