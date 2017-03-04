32c6d8a98e19e2c2d701532a5dcbc5205b78b414
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:31:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox ўяўляе сабой воблака на базе захоўвання файлаў і сінхранізацыі дадзеных. Выкарыстоўвайце гэта дадатак для сінхранізацыі файлаў у тэчцы, размешчанай у вашым хатнім каталогу, які затым можна атрымаць доступ з дапамогай любой прылады, звязанага з той жа ўліковага запісу Dropbox (ноўтбук, мабільны, таблеткі і г.д.).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Аўтэнтыфікаваных з Dropbox';
+$lang['user_dropbox_authentication'] = 'Поўная налада з дапамогай аўтэнтыфікацыі з Dropbox';
+$lang['user_dropbox_create_account'] = 'Стварыць уліковы запіс Dropbox';
+$lang['user_dropbox_have_account'] = 'У мяне ўжо ёсць уліковы запіс Dropbox';
+$lang['user_dropbox_init_complete'] = 'Ініцыялізацыя завершана. Калі ласка, націсніце на спасылку ніжэй, каб выканаць аўтарызацыю Dropbox на свой рахунак.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Вам спатрэбіцца уліковы запіс Dropbox для сінхранізацыі тэчкі лакальнага сервера ў хмарнай службе.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Цяпер, калі ў вас ёсць вашыя налады ўліковага запісу Dropbox, націсніце на кнопку "Сінхранізаваць". Вам будзе прадастаўлены з URL, які змяшчае ключ да тэчцы Dropbox, якія пражываюць у вашым хатнім каталогу. Выкарыстоўвайце гэтую спасылку, каб звязаць свой уліковы запіс Dropbox з гэтым прызначэння, пстрыкнуўшы па ім і завяршэння аўтарызацыі Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Кошт з&#39;яўляецца несапраўдным.';
+$lang['user_dropbox_not_available_for_root_account'] = 'На жаль, сэрвіс Dropbox недаступны для каранёвай ўліковага запісу.';
+$lang['user_dropbox_starting_account_init'] = 'Запуск ініцыялізацыі ўліковага запісу ... Калі ласка, будзьце цярплівыя.';
+$lang['user_dropbox_step_1'] = 'Крок 1 з 2';
+$lang['user_dropbox_step_2'] = 'Крок 2 з 2';
+$lang['user_dropbox_sync_check'] = 'праверка сінхранізацыі';
+$lang['user_dropbox_sync_now'] = 'сінхранізаваць зараз';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Спасылка';
+$lang['user_dropbox_waiting_confirmation'] = 'Чаканне аўтэнтыфікацыі сінхранізацыі ўліковага запісу Dropbox для завяршэння. Калі ласка, націсніце на Dropbox URL спасылку ніжэй, калі вы не наведвалі сайт Dropbox і звязалі свой рахунак.';
