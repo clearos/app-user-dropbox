6b93a2eb37ec776753e9d9189b416267145aac4c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:36:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox ndi mtambo ofotokoza file yosungirako ndi utumiki kalunzanitsidwe. Ntchito pulogalamu kuti synchronize owona kuti chikwatu inali ku nyumba Directory wanu kenako kufika ndi chipangizo chilichonse chogwirizana ndi chimodzimodzi Dropbox nkhani (laputopu, mafoni, piritsi etc.).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Kutsimikizira ndi Dropbox';
+$lang['user_dropbox_authentication'] = 'Complete khwekhwe ndi authenticating ndi Dropbox';
+$lang['user_dropbox_create_account'] = 'Kulenga nkhani Dropbox';
+$lang['user_dropbox_have_account'] = 'I kale ndi nkhani Dropbox';
+$lang['user_dropbox_init_complete'] = 'Initialization wathunthu. Chonde alemba pa kugwirizana m&#39;munsimu kumaliza Dropbox chilolezo ku ciwerengero canu.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Muyenera nkhani Dropbox kuti synchronize makina wanu chikwatu kumeneko ku msonkhano mtambo ofotokoza.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Tsopano muli nkhani Dropbox wanu khwekhwe, alemba pa "synchronize tsopano" batani. Inu kupatsidwa ulalo muli chinsinsi fodayi Dropbox akukhala m&#39;nyumba Directory wanu. Ntchito kugwirizana kucheza nkhani yanu Dropbox ndi malowa mwa kuwonekera pa izo ndi kumaliza chilolezo Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Nkhani ili udzakhala osazindikilika.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Pepani, utumiki Dropbox palibe chifukwa nkhaniyo mizu.';
+$lang['user_dropbox_starting_account_init'] = 'Kuyambira nkhani initialization ... chonde pirira.';
+$lang['user_dropbox_step_1'] = 'Gawo 1 of 2';
+$lang['user_dropbox_step_2'] = 'Gawo 2 of 2';
+$lang['user_dropbox_sync_check'] = 'Chongani kulunzanitsa';
+$lang['user_dropbox_sync_now'] = 'synchronize Tsopano';
+$lang['user_dropbox_url_link'] = 'Dropbox ulalo Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Kudikira Dropbox kutsimikizika nkhani kalunzanitsidwe kumaliza. Chonde alemba pa Dropbox ulalo Link m&#39;munsimu ngati simunakhala anapita malo Dropbox ndi zogwirizana nkhani yanu.';
