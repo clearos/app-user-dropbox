ca6689b0c8f5d8a4de6adc11082be3d942cfcb42
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:38:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox je ukládání souborů cloud-based a synchronizační službu. Pomocí této aplikace synchronizovat soubory do složky umístěné ve vašem domovském adresáři, které pak mohou být přístupné jakéhokoliv zařízení spojeného ke stejnému účtu Dropbox (notebook, mobilní telefon, tablet atd.).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Ověření s Dropbox';
+$lang['user_dropbox_authentication'] = 'Kompletní nastavení ověřováním s Dropbox';
+$lang['user_dropbox_create_account'] = 'Vytvořit účet Dropbox';
+$lang['user_dropbox_have_account'] = 'Už mám účet Dropbox';
+$lang['user_dropbox_init_complete'] = 'Inicializace dokončena. Klikněte prosím na odkaz níže k dokončení oprávnění Dropbox ke svému účtu.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Budete potřebovat účet Dropbox k synchronizaci místní složku serveru ke službě na principu cloudů.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Nyní, když máte nastavení svého účtu Dropbox, klikněte na tlačítko "Synchronizovat". Ty budou poskytnuty s URL, který obsahuje klíč ke složce Dropbox bydlištěm ve vašem domovském adresáři. Pomocí tohoto odkazu spojovat svůj Dropbox účet této destinace kliknutím na něj a dokončení povolení Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Účet je neplatný.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Je nám líto, Dropbox služba není dostupná pro účet root.';
+$lang['user_dropbox_starting_account_init'] = 'Spuštění inicializace účtu ... buďte trpěliví.';
+$lang['user_dropbox_step_1'] = 'Krok 1 ze 2';
+$lang['user_dropbox_step_2'] = 'Krok 2 ze 2';
+$lang['user_dropbox_sync_check'] = 'Check Sync';
+$lang['user_dropbox_sync_now'] = 'synchronizace teď';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Čekání na ověření synchronizace účtu Dropbox dokončit. Kliknutím na Dropbox URL odkaz níže, pokud jste navštívili Dropbox a propojili svůj účet.';
