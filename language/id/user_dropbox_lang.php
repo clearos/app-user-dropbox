ba84e35d528bef3eee44dd257f4ba975f42c17a7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:49:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox adalah penyimpanan file berbasis cloud dan layanan sinkronisasi. Gunakan aplikasi ini untuk menyinkronkan file ke folder yang terletak di direktori home Anda yang kemudian dapat diakses oleh setiap perangkat yang terkait dengan akun Dropbox yang sama (laptop, ponsel, tablet dll).';
+$lang['user_dropbox_app_name'] = 'dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Otentikasi dengan Dropbox';
+$lang['user_dropbox_authentication'] = 'Lengkap setup oleh otentikasi dengan Dropbox';
+$lang['user_dropbox_create_account'] = 'Buat akun Dropbox';
+$lang['user_dropbox_have_account'] = 'Saya sudah memiliki akun Dropbox';
+$lang['user_dropbox_init_complete'] = 'Inisialisasi selesai. Silakan klik pada link di bawah untuk menyelesaikan otorisasi Dropbox ke akun Anda.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Anda akan memerlukan account Dropbox untuk melakukan sinkronisasi folder server lokal untuk layanan berbasis cloud.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Sekarang bahwa Anda memiliki setup account Dropbox Anda, klik pada "Synchronize sekarang" tombol. Anda akan diberikan dengan URL yang berisi kunci untuk Dropbox folder yang berada di direktori home Anda. Gunakan link ini untuk mengaitkan akun Dropbox Anda dengan tujuan ini dengan mengkliknya dan menyelesaikan otorisasi Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Akun tidak valid.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Maaf, layanan Dropbox tidak tersedia untuk account root.';
+$lang['user_dropbox_starting_account_init'] = 'Mulai inisialisasi akun ... harap bersabar.';
+$lang['user_dropbox_step_1'] = 'Langkah 1 dari 2';
+$lang['user_dropbox_step_2'] = 'Langkah 2 dari 2';
+$lang['user_dropbox_sync_check'] = 'periksa Sync';
+$lang['user_dropbox_sync_now'] = 'sinkronisasi Sekarang';
+$lang['user_dropbox_url_link'] = 'Dropbox URL link';
+$lang['user_dropbox_waiting_confirmation'] = 'Menunggu Dropbox otentikasi sinkronisasi akun untuk menyelesaikan. Silakan klik pada Dropbox URL link di bawah ini jika Anda belum mengunjungi situs Dropbox dan terkait akun Anda.';
