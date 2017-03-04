853bb4169f9a186e6a567d14e735e3a5a3618cbb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:47:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox hadari ne mai tushen fayil ajiya da kuma aiki tare da sabis na. Yi amfani da wannan app don aiki tare da fayiloli zuwa babban fayil located a cikin gida directory wanda sannan za a iya isa ga ta kowane na&#39;urar hade da wannan Dropbox account (kwamfyutan, mobile, kwamfutar hannu da dai sauransu).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Gaskata da Dropbox';
+$lang['user_dropbox_authentication'] = 'Complete saitin da tantancewa tare da Dropbox';
+$lang['user_dropbox_create_account'] = 'Create Dropbox account';
+$lang['user_dropbox_have_account'] = 'Na riga da Dropbox account';
+$lang['user_dropbox_init_complete'] = 'Initialization complete. Don Allah danna kan mahada a kasa don kammala Dropbox izni to your account.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Za ka bukatar wani Dropbox account don aiki tare da na gida uwar garken fayil zuwa girgije tushen sabis.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Yanzu da ka da ka Dropbox account saitin, danna kan "aiki tare Yanzu" button. Za a bayar da wani URL dauke da wani key da Dropbox fayil zaune a cikin gida directory. Yi amfani da wannan link to shirki your Dropbox account tare da wannan manufa ta danna kan shi da kuma kammala Dropbox izni.';
+$lang['user_dropbox_invalid_account'] = 'Account ba daidai ba ne.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Yi haƙuri, Dropbox sabis ba don tushen account.';
+$lang['user_dropbox_starting_account_init'] = 'An fara account initialization ... don Allah ku yi haƙuri.';
+$lang['user_dropbox_step_1'] = 'Mataki 1 of 2';
+$lang['user_dropbox_step_2'] = 'Mataki 2 of 2';
+$lang['user_dropbox_sync_check'] = 'duba Sync';
+$lang['user_dropbox_sync_now'] = 'Haɗa aiki tare da Now';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Jiran Dropbox account aiki tare Tantance kalmar sirri don kammala. Don Allah danna kan Dropbox URL Link kasa idan ka ba su ziyarci Dropbox site da nasaba asusunka.';
