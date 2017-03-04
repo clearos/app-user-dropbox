82dffe9f5543fde973de82d7642683cf893b6e3f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:46:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox se yon depo dosye nwaj ki baze sou ak sèvis senkronizasyon. Sèvi ak app sa a nan senkroniz dosye nan yon katab ki sitiye nan anyè lakay ou ki ka Lè sa a jwenn aksè pa nenpòt aparèy ki asosye nan menm kont lan Dropbox (laptop, mobil, grenn elatriye).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Legalized ak Dropbox';
+$lang['user_dropbox_authentication'] = 'Ranpli konfigirasyon pa otantifye ak Dropbox';
+$lang['user_dropbox_create_account'] = 'Kreye Dropbox kont';
+$lang['user_dropbox_have_account'] = 'Mwen deja gen yon kont Dropbox';
+$lang['user_dropbox_init_complete'] = 'Inisyalizasyon konplè. Tanpri klike sou lyen ki anba a ranpli otorizasyon Dropbox nan kont ou.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Ou pral bezwen yon kont Dropbox nan senkroniz katab sèvè lokal ou a nan sèvis la nwaj ki baze sou.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Kounye a ke ou gen konfigirasyon kont Dropbox ou a, klike sou bouton an "Senkroniz Koulye a,". Ou yo pral bay ak yon URL ki gen yon kle nan katab la Dropbox k ap viv nan anyè lakay ou. Sèvi ak lyen sa a asosye kont Dropbox ou a ak destinasyon sa a pa klike sou li epi konplete otorizasyon an Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Kont se valab.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Padon, sèvis la Dropbox a pa disponib pou kont lan rasin.';
+$lang['user_dropbox_starting_account_init'] = 'Koumanse kont inisyalizasyon ... tanpri pran pasyans.';
+$lang['user_dropbox_step_1'] = 'Etap 1 nan 2';
+$lang['user_dropbox_step_2'] = 'Etap 2 nan 2';
+$lang['user_dropbox_sync_check'] = 'tcheke senkro';
+$lang['user_dropbox_sync_now'] = 'senkroniz Koulye a,';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Ap tann pou Dropbox senkronizasyon kont otantifikasyon yo fini. Tanpri klike sou Dropbox URL Link ki anba la a si ou pa gen te vizite sit la Dropbox ak lye kont ou.';
