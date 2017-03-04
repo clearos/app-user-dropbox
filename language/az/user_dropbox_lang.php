7f179fc60befe721b0543757aa85dd9da118b1a4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:30:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox bir bulud-based fayl storage və synchronization xidmət edir. sonra eyni Dropbox hesabı (s laptop, mobil, tablet) bağlı hər hansı bir cihaz tərəfindən istifadə edilə bilər ev kataloq yerləşən qovluğa faylları sinxronizasiya etmək üçün bu app istifadə edin.';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Dropbox ilə Orijinal';
+$lang['user_dropbox_authentication'] = 'Dropbox ilə kimlik denetimi ilə tam quraşdırma';
+$lang['user_dropbox_create_account'] = 'Dropbox hesabı yaradın';
+$lang['user_dropbox_have_account'] = 'Mən artıq bir Dropbox hesabı';
+$lang['user_dropbox_init_complete'] = 'Başlatma tam. Hesabınıza Dropbox icazə başa çatdırmaq üçün aşağıdakı linki basın.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Siz bulud-based xidmət üçün yerli server qovluq sinxronizasiya etmək üçün bir Dropbox hesabı lazımdır.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'İndi Dropbox hesabı quraşdırma ki, "İndi sinxronizasiya" düyməsinə basın. Siz ev kataloq yaşayan Dropbox qovluq əsas olan URL ilə təmin olunacaq. tıklayarak və Dropbox icazə tamamlayaraq bu təyinat ilə Dropbox hesabı birləşmək üçün bu linki istifadə edin.';
+$lang['user_dropbox_invalid_account'] = 'Account yanlışdır.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Bağışlayın, Dropbox xidmət kök hesab üçün mövcud deyil.';
+$lang['user_dropbox_starting_account_init'] = 'Hesab başlatma başlayaraq ... səbr edin.';
+$lang['user_dropbox_step_1'] = '2 Addım 1';
+$lang['user_dropbox_step_2'] = '2 Addım 2';
+$lang['user_dropbox_sync_check'] = 'Sync edin';
+$lang['user_dropbox_sync_now'] = 'İndi sinxronizasiya';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Dropbox hesabı synchronization identifikasiyası üçün gözləyir başa çatdırmaq üçün. Siz Dropbox saytı ziyarət və hesabı bağlı əgər aşağıdakı Dropbox URL linki vurun.';
