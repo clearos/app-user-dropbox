cc5cae94a850bfe861303ff7eb176defd5f47d8b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:59:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox ir mākonis balstītas failu glabāšanas un sinhronizācijas pakalpojumu. Izmantojiet šo app, lai sinhronizētu failus uz mapi, kas atrodas jūsu mājas direktorijā, kas pēc tam var piekļūt ar jebkuru ierīci, kas saistīts ar to pašu Dropbox kontu (klēpjdators, mobilo, tabletes uc).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Autentificēšana ar Dropbox';
+$lang['user_dropbox_authentication'] = 'Complete setup autentiskumu ar Dropbox';
+$lang['user_dropbox_create_account'] = 'Izveidot Dropbox kontu';
+$lang['user_dropbox_have_account'] = 'Man jau ir Dropbox kontu';
+$lang['user_dropbox_init_complete'] = 'Inicializācijas pabeigta. Lūdzu, noklikšķiniet uz saites zemāk, lai pabeigtu Dropbox atļauju savā kontā.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Jums būs nepieciešams Dropbox kontu, lai sinhronizētu savu vietējo servera mapi mākoņa arhitektūras pakalpojumu.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Tagad, ka jums ir jūsu Dropbox konta iestatīšanu, noklikšķiniet uz "sinhronizēt Now" pogu. Jums tiks nodrošināti ar URL satur atslēgu uz Dropbox mapi dzīvo jūsu mājas direktorijā. Izmantojiet šo saiti, lai piesaistītu jūsu Dropbox kontu ar šo galamērķi, noklikšķinot uz tā un aizpildot Dropbox atļauju.';
+$lang['user_dropbox_invalid_account'] = 'Konts ir nederīgs.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Atvainojiet, Dropbox pakalpojums nav pieejams saknes kontā.';
+$lang['user_dropbox_starting_account_init'] = 'Sākot konta inicializācijas ... lūdzu, esiet pacietīgi.';
+$lang['user_dropbox_step_1'] = '1. solis no 2';
+$lang['user_dropbox_step_2'] = '2 no 2 Step';
+$lang['user_dropbox_sync_check'] = 'pārbaudiet Sync';
+$lang['user_dropbox_sync_now'] = 'sinhronizēt tagad';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Gaida Dropbox kontu sinhronizācijas autentifikāciju, lai pabeigtu. Lūdzu, noklikšķiniet uz Dropbox URL saite zemāk, ja jūs neesat apmeklējis Dropbox vietni un saistīts savu kontu.';
