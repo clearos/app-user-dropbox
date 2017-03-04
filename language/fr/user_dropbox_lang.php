4f56181776358ea9de12fc2d19089186422cc0e0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:42:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox est un stockage de fichiers en nuage et d&#39;un service de synchronisation. Utilisez cette application pour synchroniser les fichiers dans un dossier situé dans votre répertoire personnel qui peut ensuite être consulté par tout dispositif associé au même compte Dropbox (ordinateur portable, mobile, tablette, etc.).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Authentifier avec Dropbox';
+$lang['user_dropbox_authentication'] = 'Terminer la configuration en authentifiant avec Dropbox';
+$lang['user_dropbox_create_account'] = 'Créez un compte Dropbox';
+$lang['user_dropbox_have_account'] = 'j&#39;ai déjà un compte Dropbox';
+$lang['user_dropbox_init_complete'] = 'Initialisation complète. S&#39;il vous plaît cliquer sur le lien ci-dessous pour compléter l&#39;autorisation de Dropbox sur votre compte.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Vous aurez besoin d&#39;un compte Dropbox pour synchroniser votre dossier de serveur local pour le service en nuage.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Maintenant que vous avez la configuration de votre compte Dropbox, cliquez sur le bouton "Synchroniser maintenant". Il vous sera fourni avec une URL contenant une clé dans le dossier Dropbox résidant dans votre répertoire personnel. Utilisez ce lien pour associer votre compte Dropbox avec cette destination en cliquant dessus et en complétant l&#39;autorisation Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Le compte est invalide.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Désolé, le service Dropbox ne sont pas disponibles pour le compte root.';
+$lang['user_dropbox_starting_account_init'] = 'Démarrage compte initialisation ... s&#39;il vous plaît être patient.';
+$lang['user_dropbox_step_1'] = 'Étape 1 de 2';
+$lang['user_dropbox_step_2'] = 'Étape 2 de 2';
+$lang['user_dropbox_sync_check'] = 'Contrôle Sync';
+$lang['user_dropbox_sync_now'] = 'Synchroniser maintenant';
+$lang['user_dropbox_url_link'] = 'Dropbox Lien URL';
+$lang['user_dropbox_waiting_confirmation'] = 'En attente de l&#39;authentification de synchronisation de compte Dropbox pour terminer. S&#39;il vous plaît cliquer sur le lien ci-dessous Dropbox URL si vous ne l&#39;avez pas visité le site Dropbox et lié votre compte.';
