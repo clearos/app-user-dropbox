73f14daec4b3f0a24477ed6643bfed87308b37bd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:50:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Is Dropbox stórála comhad scamall-bhunaithe agus seirbhís sioncrónaithe. Bain úsáid as an app chun comhaid sioncrónú i bhfillteán atá lonnaithe i do chomhadlann bhaile ar féidir teacht ansin ag aon fheiste bainteach leis an gcuntas Dropbox céanna (ríomhaire glúine, soghluaiste, tablet srl).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Fhíordheimhniú le Dropbox';
+$lang['user_dropbox_authentication'] = 'thus Críochnaithe faoi a fhíordheimhniú le Dropbox';
+$lang['user_dropbox_create_account'] = 'Cruthaigh cuntas Dropbox';
+$lang['user_dropbox_have_account'] = 'bhfuil cuntas Dropbox agam cheana';
+$lang['user_dropbox_init_complete'] = 'Initialization iomlán. Cliceáil ar an nasc thíos údarú Dropbox a chur i gcrích do chuntas.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Beidh ort cuntas Dropbox chun sioncrónú do fhillteán fhreastalaí áitiúil leis an tseirbhís scamall-bhunaithe.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Anois go bhfuil tú do thus chuntas Dropbox, cliceáil ar an gcnaipe "Sioncrónaigh Anois". Beidh tú a chur ar fáil le URL ina bhfuil eochair chun an fillteán Dropbox bhfuil cónaí orthu i do eolaire bhaile. Úsáid an nasc seo a chomhlachú do chuntas Dropbox leis an gceann scríbe trí chliceáil ar é agus an t-údarú Dropbox chomhlánú.';
+$lang['user_dropbox_invalid_account'] = 'Is Cuntas neamhbhailí.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Tá brón orainn, nach bhfuil an tseirbhís Dropbox ar fáil do chuntas root.';
+$lang['user_dropbox_starting_account_init'] = 'Ag tosú initialization cuntas ... le do thoil a n-othar.';
+$lang['user_dropbox_step_1'] = 'Céim 1 de 2';
+$lang['user_dropbox_step_2'] = 'Céim 2 de 2';
+$lang['user_dropbox_sync_check'] = 'seiceáil Sioncrónaigh';
+$lang['user_dropbox_sync_now'] = 'Sioncrónaigh Anois';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Ag feitheamh le Dropbox fíordheimhniú cuntais sioncrónaithe a chur i gcrích. Cliceáil ar an URL Link Dropbox thíos mura bhfuil cuairt ar an suíomh Dropbox agus nasctha do chuntas.';
