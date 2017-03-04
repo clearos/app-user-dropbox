7db3015b2dddf866f7f55319942dbdb965c44877
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:54:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox бұлтқа негізделген файл сақтау және синхрондау қызметі болып табылады. Содан кейін сол Dropbox шотына (ноутбук, ұялы, планшет және т.б.) байланысты кез келген құрылғы арқылы ашуға болады сіздің үй каталогында орналасқан қалтаға файлдарды синхрондау үшін осы қолданбаны пайдаланыңыз.';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Dropbox түпнұсқалығын растайтын';
+$lang['user_dropbox_authentication'] = 'Dropbox түпнұсқалығын растауға толық орнату';
+$lang['user_dropbox_create_account'] = 'Dropbox тіркелгісін жасау';
+$lang['user_dropbox_have_account'] = 'Мен қазірдің өзінде Dropbox тіркелгісі бар';
+$lang['user_dropbox_init_complete'] = 'Баптандыру толық. тіркелгіңізге Dropbox рұқсатын аяқтау үшін төмендегі сілтемені басыңыз.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Сіз бұлтқа негізделген қызметке сіздің жергілікті сервер қалтасын қадамдастыру үшін Dropbox тіркелгісін қажет болады.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Енді сіз өз Dropbox тіркелгі орнатуды бар екенін, «Қазірқадамдастыру» батырмасын басыңыз. Сіз сіздің үй каталогында тұратын Dropbox қалтаға пернені бар URL мекенжайына қамтамасыз етіледі. оған басу және Dropbox авторизацияны аяқтау арқылы осы межелі Сіздің Dropbox тіркелгісін байланыстыру үшін, осы сілтемені пайдаланыңыз.';
+$lang['user_dropbox_invalid_account'] = 'Шот жарамсыз болып табылады.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Кешіріңіз, Dropbox қызмет түбірлік тіркелгі үшін қол жетімді емес.';
+$lang['user_dropbox_starting_account_init'] = 'тіркелгі баптандыруды бастап ... өтінеміз шыдамды.';
+$lang['user_dropbox_step_1'] = '2 1-қадам';
+$lang['user_dropbox_step_2'] = '2 2-қадам';
+$lang['user_dropbox_sync_check'] = 'Sync тексеріңіз';
+$lang['user_dropbox_sync_now'] = 'Енді синхрондау';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Сілтеме';
+$lang['user_dropbox_waiting_confirmation'] = 'аяқтау үшін Dropbox тіркелгісі үндестіру түпнұсқалығын растау үшін күту. Сіз Dropbox сайты барып, тіркелгіңізді байланысты емес болса, төменде Dropbox URL сілтемені басыңыз сұраймыз.';
