fc73d27308315ba21ce0dcca603c45614ac4823b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:35:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox ကိုမိုဃ်းတိမ်-based ဖိုင်သိုလှောင်မှုနဲ့ synchronize လုပ်ပေးတဲ့ service တစ်ခုဖြစ်ပါသည်။ ထို့နောက်တူညီသော Dropbox အကောင့်ကို (Laptop ကို, မိုဘိုင်း, တက်ဘလက် etc) မှဆက်စပ်မည်သည့် device ကိုအားဖြင့်ဝင်ရောက်နိုင်သည့်သင့်အိမ် directory ထဲတွင်တည်ရှိပြီး folder တစ်ခုဖိုင်များကိုတွေကိုတပြိုင်တည်းအလုပ်လုပ်မှဒီ app ကိုအသုံးပြုပါ။';
+$lang['user_dropbox_app_name'] = 'Dropbox ကို';
+$lang['user_dropbox_authenticate_to_service'] = 'Dropbox ကိုနှင့်အတူစစ်မှန်ကြောင်းအတည်ပြု';
+$lang['user_dropbox_authentication'] = 'Dropbox ကိုနှင့်အတူစစ်မှန်အားဖြင့်ပြီးပြည့်စုံသော setup ကို';
+$lang['user_dropbox_create_account'] = 'Dropbox အကောင့်ကို Create';
+$lang['user_dropbox_have_account'] = 'ငါပြီးသား Dropbox အကောင့်';
+$lang['user_dropbox_init_complete'] = 'မိတ်ဆက်ပြီးပြည့်စုံ။ သင့်အကောင့်ကို Dropbox ခွင့်ပြုချက်ဖြည့်စွက်ဖို့အောက်က link ကို click နှိပ်ပြီးပေးပါ။';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'သင်က cloud အခြေခံဝန်ဆောင်မှုရန်သင်၏ဒေသဆိုင်ရာ server က folder ကိုတွေကိုတပြိုင်တည်းအလုပ်လုပ်ဖို့ Dropbox အကောင့်ကိုလိုအပ်ပါလိမ့်မည်။';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'ယခုသင်သည်သင်၏ Dropbox အကောင့်ကို setup ကိုရှိသည်သော, အ "အခုဆိုရင်ထပ်တူကျအောင်" button ကို click လုပ်ပါ။ သငျသညျသငျ့အိမျ directory ထဲတွင်နေထိုင်သည့် Dropbox ဖိုင်တွဲရန်သော့ချက်င်တစ်ဦး URL ကိုပေးလိမ့်မည်။ ဒါကြောင့်အပေါ်ကိုနှိပ်ခြင်းနှင့် Dropbox ကိုခွင့်ပြုချက်ဖြည့်စွက်အားဖြင့်ဒီဦးတည်ရာနှင့်သင်၏ Dropbox အကောင့်ကိုပေါင်းသင်းဖို့ဒီ link ကိုအသုံးပြုပါ။';
+$lang['user_dropbox_invalid_account'] = 'အကောင့်မမှန်ကန်ဘူး။';
+$lang['user_dropbox_not_available_for_root_account'] = 'စိတ်မကောင်းပါ, Dropbox ကိုဝန်ဆောင်မှုအမြစ်အကောင့်အတွက်ရရှိနိုင်ပါသည်မဟုတ်။';
+$lang['user_dropbox_starting_account_init'] = 'အကောင့်စတင်ခြင်းစတင်ကာ ... လူနာဖြစ်ပါ။';
+$lang['user_dropbox_step_1'] = '2 အဆင့် 1';
+$lang['user_dropbox_step_2'] = '2 အဆင့် 2';
+$lang['user_dropbox_sync_check'] = 'Sync ကို Check';
+$lang['user_dropbox_sync_now'] = 'အခုတော့ထပ်တူကျအောင်';
+$lang['user_dropbox_url_link'] = 'Dropbox ကို URL ကို Link ကို';
+$lang['user_dropbox_waiting_confirmation'] = 'ဖြည့်စွက်ဖို့ Dropbox အကောင့်ကို synchronization authentication ကိုစောင့်ဆိုင်းနေသည်။ သင် Dropbox ကိုဆိုက်သွားရောက်ခဲ့ပြီးနှင့်သင့်အကောင့်နှင့်ဆက်စပ်ကြပြီမဟုတ်လျှင်အောက်တွင်ဖော်ပြထားသော Dropbox ကို URL ကို Link ကိုအပေါ်ကိုကလစ်နှိပ်ပါပေးပါ။';
