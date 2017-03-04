86fdcfe16f1ac28e87859224053aa6ac28af4d90
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:45:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox είναι μια cloud-based αποθήκευσης αρχείων και υπηρεσία συγχρονισμού. Χρησιμοποιήστε αυτό το app για να συγχρονίσετε τα αρχεία σε ένα φάκελο που βρίσκεται στο home directory σας, η οποία μπορεί στη συνέχεια να προσεγγιστεί από οποιαδήποτε συσκευή που συνδέεται στον ίδιο λογαριασμό Dropbox (laptop, κινητό, tablet κ.λπ.).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Έλεγχος ταυτότητας με Dropbox';
+$lang['user_dropbox_authentication'] = 'Πλήρης εγκατάσταση με έλεγχο ταυτότητας με Dropbox';
+$lang['user_dropbox_create_account'] = 'Δημιουργία λογαριασμού Dropbox';
+$lang['user_dropbox_have_account'] = 'Έχω ήδη ένα λογαριασμό Dropbox';
+$lang['user_dropbox_init_complete'] = 'Αρχικοποίηση ολοκληρωθεί. Παρακαλώ κάντε κλικ στον παρακάτω σύνδεσμο για να ολοκληρώσετε άδεια Dropbox λογαριασμό σας.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Θα χρειαστείτε ένα λογαριασμό Dropbox για να συγχρονίσετε τοπικό φάκελο του διακομιστή σας με την υπηρεσία που βασίζεται σε σύννεφο.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Τώρα που έχετε ρυθμίσει το λογαριασμό σας στο Dropbox, κάντε κλικ στο κουμπί "Συγχρονισμός τώρα". Θα πρέπει να παρέχονται με μια διεύθυνση URL που περιέχει ένα κλειδί για το φάκελο Dropbox που κατοικούν στον προσωπικό σας κατάλογο. Χρησιμοποιήστε αυτό το σύνδεσμο για να συνδέσει το λογαριασμό σας στο Dropbox με αυτόν τον προορισμό κάνοντας κλικ σε αυτό και ολοκληρώνοντας την άδεια Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Ο λογαριασμός δεν είναι έγκυρο.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Συγγνώμη, η υπηρεσία Dropbox δεν είναι διαθέσιμη για το λογαριασμό root.';
+$lang['user_dropbox_starting_account_init'] = 'Ξεκινώντας λογαριασμό προετοιμασίας ... παρακαλώ να είστε υπομονετικοί.';
+$lang['user_dropbox_step_1'] = 'Βήμα 1 από 2';
+$lang['user_dropbox_step_2'] = 'Βήμα 2 από 2';
+$lang['user_dropbox_sync_check'] = 'Ελέγξτε Sync';
+$lang['user_dropbox_sync_now'] = 'Συγχρονίστε Τώρα';
+$lang['user_dropbox_url_link'] = 'Dropbox URL συνδέσμου';
+$lang['user_dropbox_waiting_confirmation'] = 'Αναμονή για Dropbox ταυτότητας συγχρονισμό λογαριασμό για να ολοκληρωθεί. Παρακαλώ κάντε κλικ στο Dropbox URL παρακάτω σύνδεσμο αν δεν έχετε επισκεφθεί την ιστοσελίδα Dropbox και συνδέονται με το λογαριασμό σας.';
