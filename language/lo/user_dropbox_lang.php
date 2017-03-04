c8571c48a8028626770522ad356471fd76cf4c0c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:58:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox ເປັນການເກັບຮັກສາໄຟລ໌ຟັງທີ່ແລະການບໍລິການ synchronization. ການນໍາໃຊ້ app ນີ້ເພື່ອ synchronize ໄຟລ໌ໄປຫາໂຟນເດີທີ່ຕັ້ງຢູ່ໃນລະບົບເຮືອນຂອງທ່ານທີ່ສາມາດຫຼັງຈາກນັ້ນຈະໄດ້ຮັບການເຂົ້າເຖິງໄດ້ໂດຍອຸປະກອນຕ່າງໆທີ່ກ່ຽວຂ້ອງກັບບັນຊີ Dropbox ດຽວກັນ (ຄອມພິວເຕີ, ໂທລະສັບມືຖື, ແທັບເລັດແລະອື່ນໆ).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'ກວດສອບກັບ Dropbox';
+$lang['user_dropbox_authentication'] = 'ການຕິດຕັ້ງສໍາເລັດສົມບູນໂດຍ authenticating ກັບ Dropbox';
+$lang['user_dropbox_create_account'] = 'ສ້າງບັນຊີ Dropbox';
+$lang['user_dropbox_have_account'] = 'ຂ້າພະເຈົ້າມີບັນຊີ Dropbox ແລ້ວ';
+$lang['user_dropbox_init_complete'] = 'ການເລີ່ມຕົ້ນສໍາເລັດສົມບູນ. ກະລຸນາຄລິກໃສ່ການເຊື່ອມຕໍ່ຂ້າງລຸ່ມນີ້ເພື່ອໃຫ້ສໍາເລັດການອະນຸຍາດ Dropbox ບັນຊີຂອງທ່ານ.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'ທ່ານຈະຕ້ອງມີບັນຊີ Dropbox ການປະສານໂຟເດີຂອງເຄື່ອງແມ່ຂ່າຍໃນທ້ອງຖິ່ນຂອງທ່ານການບໍລິການຟັງທີ່ອີງໃສ່.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'ໃນປັດຈຸບັນວ່າທ່ານມີການຕິດຕັ້ງບັນຊີ Dropbox ຂອງທ່ານ, ໃຫ້ຄລິກໃສ່ "ປະສານໃນປັດຈຸບັນ" ປຸ່ມ. ທ່ານຈະໄດ້ຮັບການສະຫນອງໃຫ້ມີ URL ທີ່ມີສໍາຄັນຕໍ່ກັບໂຟນເດີ Dropbox ທີ່ອາໄສຢູ່ໃນລະບົບເຮືອນຂອງທ່ານ. ການນໍາໃຊ້ການເຊື່ອມຕໍ່ນີ້ເພື່ອເຊື່ອມໂຍງບັນຊີ Dropbox ຂອງທ່ານທີ່ມີຈຸດຫມາຍປາຍທາງນີ້ໂດຍການຄລິກໃສ່ມັນແລະສໍາເລັດການອະນຸຍາດ Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'ບັນຊີບໍ່ຖືກຕ້ອງ.';
+$lang['user_dropbox_not_available_for_root_account'] = 'ຂໍອະໄພ, ການບໍລິການ Dropbox ແມ່ນບໍ່ສາມາດໃຊ້ສໍາລັບການບັນຊີຂອງຮາກ.';
+$lang['user_dropbox_starting_account_init'] = 'ເລີ່ມຕົ້ນເລີ່ມຕົ້ນບັນຊີ ... ກະລຸນາຄວາມອົດທົນ.';
+$lang['user_dropbox_step_1'] = 'ຂັ້ນຕອນທີ 1 ຂອງ 2';
+$lang['user_dropbox_step_2'] = 'ຂັ້ນຕອນທີ່ 2 ຈາກທັງຫມົດ 2';
+$lang['user_dropbox_sync_check'] = 'ກວດສອບ Sync';
+$lang['user_dropbox_sync_now'] = 'ປະສານໃນປັດຈຸບັນ';
+$lang['user_dropbox_url_link'] = 'URL Link Dropbox';
+$lang['user_dropbox_waiting_confirmation'] = 'ລໍຖ້າສໍາລັບການກວດສອບການປະສານບັນຊີ Dropbox ເພື່ອໃຫ້ສໍາເລັດ. ກະລຸນາຄລິກໃສ່ URL Link Dropbox ຂ້າງລຸ່ມນີ້ຖ້າຫາກວ່າທ່ານຍັງບໍ່ທັນໄດ້ໄປຢ້ຽມຢາມເວັບໄຊ Dropbox ແລະເຊື່ອມຕໍ່ບັນຊີຂອງທ່ານ.';
