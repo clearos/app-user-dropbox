4667815f2cfbd436fa73ca7ac000107392f6dd9d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:51:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox er ský-undirstaða skrá geymsla og samstillingu þjónustu. Notaðu þetta forrit til að samstilla skrár í möppu staðsett í möppu heimili þínu sem er síðan hægt að nálgast með hvaða tæki sem tengist sama Dropbox reikning (fartölvu, farsíma, tafla o.fl.).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Staðfesta með Dropbox';
+$lang['user_dropbox_authentication'] = 'Complete skipulag með staðreyna með Dropbox';
+$lang['user_dropbox_create_account'] = 'Búa Dropbox reikning';
+$lang['user_dropbox_have_account'] = 'Ég hef nú þegar Dropbox reikning';
+$lang['user_dropbox_init_complete'] = 'Frumstilling lokið. Vinsamlegast smelltu á tengilinn hér að neðan til að ljúka Dropbox heimild á reikninginn þinn.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Þú vilja þörf a Dropbox reikning til að samstilla heimamaður framreiðslumaður möppu til ský-undirstaða þjónusta.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Nú þegar þú hefur skipulag Dropbox reikninginn þinn, smelltu á "Samstilla núna" hnappinn. Þú verður að vera með slóð sem inniheldur lykil að Dropbox möppu búsettir á þínu heimasvæði. Notaðu þennan tengil til að tengja Dropbox reikninginn þinn með þessum áfangastað með því að smella á það og klára Dropbox leyfi.';
+$lang['user_dropbox_invalid_account'] = 'Reikningur er ógilt.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Því miður, Dropbox þjónusta er ekki í boði fyrir kerfisstjóra.';
+$lang['user_dropbox_starting_account_init'] = 'Byrjar reikning frumstilling ... vinsamlegast sýndu þolinmæði.';
+$lang['user_dropbox_step_1'] = 'Skref 1 af 2';
+$lang['user_dropbox_step_2'] = 'Skref 2 af 2';
+$lang['user_dropbox_sync_check'] = 'Athugaðu Sync';
+$lang['user_dropbox_sync_now'] = 'Samstilling Nú';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Bíð eftir Dropbox samstillingu auðkenningar til að ljúka. Vinsamlegast smelltu á Dropbox URL hlekkur hér að neðan ef þú hefur ekki heimsótt Dropbox síðuna og tengd reikninginn þinn.';
