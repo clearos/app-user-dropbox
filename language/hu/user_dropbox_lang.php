b9145099884622691bb7a6a5d44022cc9d3e2f39
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:48:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox egy felhő-alapú fájl tárolási és szinkronizálási szolgáltatásra. Ezzel app, hogy szinkronizálja a fájlokat egy mappába található a home könyvtár, amely azután nem férhetnek hozzá tartozó készülék ugyanarra a Dropbox fiók (laptop, mobiltelefon, tablet, stb.)';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Hitelesítés Dropbox';
+$lang['user_dropbox_authentication'] = 'Teljes beállítás hitelesíti Dropbox';
+$lang['user_dropbox_create_account'] = 'Létrehozása Dropbox fiók';
+$lang['user_dropbox_have_account'] = 'Már van egy Dropbox fiók';
+$lang['user_dropbox_init_complete'] = 'Inicializálás befejeződött. Kérjük, kattintson az alábbi linkre, hogy teljes Dropbox engedély fiókjába.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Szükséged lesz egy Dropbox fiókot, hogy szinkronizálja a helyi szerver mappából a felhő-alapú szolgáltatás.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Most, hogy a Dropbox fiók beállításának kattintson a "Szinkronizálás most" gombra. Meg kell adni az URL tartalmazza a kulcs a Dropbox mappában lakó a saját könyvtárban. Használja ezt a linket társítani Dropbox fiók célhelylistával, kattintson rá, és a bejelentkező Dropbox engedélyt.';
+$lang['user_dropbox_invalid_account'] = 'Fiók érvénytelen.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Sajnos a Dropbox szolgáltatás nem áll rendelkezésre a root fiók.';
+$lang['user_dropbox_starting_account_init'] = 'Kezdve fiók inicializálása ... kérjük, legyen türelemmel.';
+$lang['user_dropbox_step_1'] = '1. lépés a 2';
+$lang['user_dropbox_step_2'] = '2. lépés 2';
+$lang['user_dropbox_sync_check'] = 'Ellenőrizze szinkronizálása';
+$lang['user_dropbox_sync_now'] = 'Szinkronizálás most';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Várakozás Dropbox fiók szinkronizálási hitelesítés elvégzéséhez. Kérjük, kattintson a Dropbox URL Link alább, ha még nem járt a Dropbox oldalon, és kapcsolódik a fiókjába.';
