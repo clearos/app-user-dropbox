3edf61f0ba7fb6afacda2ce5eb0d75628e66ac1a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:26:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox is &#39;n wolk-gebaseerde lêer stoor en sinchronisasie diens. Gebruik hierdie inligting om lêers te sinchroniseer om &#39;n gids in jou huis gids wat dan kan verkry word deur &#39;n toestel wat verband hou met dieselfde Dropbox rekening (laptop, selfoon, tablet ens).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Authentic met Dropbox';
+$lang['user_dropbox_authentication'] = 'Volledige opstel deur waarmerking met Dropbox';
+$lang['user_dropbox_create_account'] = 'Skep Dropbox rekening';
+$lang['user_dropbox_have_account'] = 'Ek het reeds &#39;n Dropbox rekening';
+$lang['user_dropbox_init_complete'] = 'Inisialisering voltooi. Klik asseblief op die onderstaande skakel om Dropbox magtiging te voltooi om jou rekening.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Jy sal &#39;n Dropbox rekening nodig het om jou plaaslike bediener gids sinchroniseer om die wolk-gebaseerde diens.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Nou dat jy jou Dropbox rekening opstel, klik op die "Sinkroniseer nou" knoppie. Jy sal voorsien word met &#39;n URL bevat &#39;n sleutel tot die gids Dropbox woonagtig in jou huis gids. Gebruik hierdie skakel na jou Dropbox rekening te assosieer met hierdie bestemming deur te kliek op dit en die voltooiing van die Dropbox magtiging.';
+$lang['user_dropbox_invalid_account'] = 'Rekening is ongeldig.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Jammer, die Dropbox diens is nie beskikbaar vir die wortel rekening.';
+$lang['user_dropbox_starting_account_init'] = 'Begin rekening inisialisering ... asseblief wees geduldig.';
+$lang['user_dropbox_step_1'] = 'Stap 1 van 2';
+$lang['user_dropbox_step_2'] = 'Stap 2 van 2';
+$lang['user_dropbox_sync_check'] = 'Gaan Sync';
+$lang['user_dropbox_sync_now'] = 'sinchroniseer Nou';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Wag vir Dropbox rekening sinchronisasie verifikasie te voltooi. Klik asseblief op die Dropbox URL skakel hieronder as jy nie die Dropbox webwerf besoek en gekoppel jou rekening.';
