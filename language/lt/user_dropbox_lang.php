fee8fbaa378ebd8c38845a6ede072a1187f9c3bb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:59:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'ZMI yra debesis pagrįstas failų saugykla ir sinchronizavimas tarnyba. Naudokite šią programą, sinchronizuoti failus į aplanką esantį jūsų namų katalogą, kuris vėliau gali būti prieinama bet kuriuo įrenginiu susijęs su tuo pačiu Dropbox sąskaitą (nešiojamas, mobilus, tablečių ir tt).';
+$lang['user_dropbox_app_name'] = 'ZMI';
+$lang['user_dropbox_authenticate_to_service'] = 'Atpažinkite su Dropbox';
+$lang['user_dropbox_authentication'] = 'Pilnas nustatymas autentifikuodama su Dropbox';
+$lang['user_dropbox_create_account'] = 'Sukurti Dropbox sąskaitą';
+$lang['user_dropbox_have_account'] = 'Aš jau turiu Dropbox sąskaitą';
+$lang['user_dropbox_init_complete'] = 'Pradės baigtas. Prašome paspausti žemiau esančią nuorodą užbaigti Dropbox leidimą prie savo paskyros.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Jums reikės Dropbox sąskaitą sinchronizuoti vietinio serverio aplanką į debesis pagrįstas paslaugas.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Dabar, kad jūs turite savo ZMI paskyros sąranką, spauskite "Sinchronizuoti dabar" mygtuką. Jums bus suteikta su URL, kuriame yra raktas į Dropbox aplanką gyvenančio jūsų namų katalogą. Naudokite šią nuorodą, norėdami susieti savo Dropbox sąskaitą šioje vietoje paspaudę ant jos ir užbaigti Dropbox leidimą.';
+$lang['user_dropbox_invalid_account'] = 'Sąskaita yra neteisinga.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Atsiprašome, tačiau Dropbox paslauga neprieinama šaknų sąskaitą.';
+$lang['user_dropbox_starting_account_init'] = 'Nuo sąskaitos iniciacijos ... būkite kantrūs.';
+$lang['user_dropbox_step_1'] = '1 žingsnis 2';
+$lang['user_dropbox_step_2'] = 'Žingsnis 2 iš 2';
+$lang['user_dropbox_sync_check'] = 'Patikrinkite Sync';
+$lang['user_dropbox_sync_now'] = 'sinchronizuoti dabar';
+$lang['user_dropbox_url_link'] = 'ZMI URL nuorodą';
+$lang['user_dropbox_waiting_confirmation'] = 'Laukiama Dropbox sąskaitą sinchronizavimo autentifikavimo užbaigti. žemiau, jei turite ne apsilankė Dropbox svetainę ir susiję sąskaitą Prašome paspausti ant Dropbox URL nuorodą.';
