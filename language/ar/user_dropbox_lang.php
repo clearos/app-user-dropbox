61b83698ecb94a4db07a37920cd04210ea576bf8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:28:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'دروببوإكس هو تخزين الملفات السحابية وخدمة التزامن. استخدام هذا التطبيق لمزامنة الملفات إلى مجلد يقع في الدليل الرئيسي والتي يمكن الوصول إليها من قبل أي جهاز يرتبط إلى نفس حساب دروببوإكس (الكمبيوتر المحمول، المحمول، الكمبيوتر اللوحي وغيرها).';
+$lang['user_dropbox_app_name'] = 'بصندوق الإسقاط';
+$lang['user_dropbox_authenticate_to_service'] = 'مصادقة مع Dropbox';
+$lang['user_dropbox_authentication'] = 'الإعداد الكامل من قبل مصادقة مع Dropbox';
+$lang['user_dropbox_create_account'] = 'إنشاء حساب دروببوإكس';
+$lang['user_dropbox_have_account'] = 'لدي بالفعل حساب دروببوإكس';
+$lang['user_dropbox_init_complete'] = 'تهيئة كاملة. الرجاء النقر على الرابط أدناه لإكمال عملية التخويل دروببوإكس إلى حسابك.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'سوف تحتاج إلى حساب Dropbox لمزامنة مجلد الخادم المحلي للخدمة السحابية.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'الآن أن لديك إعداد حساب دروببوإكس، انقر على زر "مزامنة الآن". وسيتم توفير لكم مع عنوان URL يحتوي على المفتاح إلى المجلد دروببوإكس المقيمين في دليل منزلك. استخدام هذا الرابط لربط حساب دروببوإكس الخاص بك مع هذه الوجهة عن طريق النقر عليه واستكمال إذن دروببوإكس.';
+$lang['user_dropbox_invalid_account'] = 'الحساب غير صالح.';
+$lang['user_dropbox_not_available_for_root_account'] = 'عذرا، خدمة دروببوإكس غير متوفرة لحساب الجذر.';
+$lang['user_dropbox_starting_account_init'] = 'بدء التهيئة حساب ... يرجى التحلي بالصبر.';
+$lang['user_dropbox_step_1'] = 'الخطوة 1 من 2';
+$lang['user_dropbox_step_2'] = 'الخطوة 2 من 2';
+$lang['user_dropbox_sync_check'] = 'تحقق مزامنة';
+$lang['user_dropbox_sync_now'] = 'مزامنة الآن';
+$lang['user_dropbox_url_link'] = 'دروببوإكس URL رابط';
+$lang['user_dropbox_waiting_confirmation'] = 'انتظار دروببوإكس المصادقة تزامن حساب لإكمال. الرجاء النقر على دروببوإكس URL رابط أدناه إذا لم تكن قد قمت بزيارة الموقع دروببوإكس وربط حسابك.';
