25d1b47151bb62a1b3c28010064b3dde7be2a268
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:56:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = '드롭 박스는 클라우드 기반 파일 저장 및 동기화 서비스입니다. 다음 같은 드롭 박스 계정 (등 노트북, 모바일, 태블릿)에 연결된 모든 장치에 액세스 할 수 있습니다 홈 디렉토리에있는 폴더에 파일을 동기화하는이 응용 프로그램을 사용합니다.';
+$lang['user_dropbox_app_name'] = '드롭 박스';
+$lang['user_dropbox_authenticate_to_service'] = '드롭 박스와 인증';
+$lang['user_dropbox_authentication'] = '보관 용으로 인증하여 전체 설치';
+$lang['user_dropbox_create_account'] = '보관 용 계정 만들기';
+$lang['user_dropbox_have_account'] = '나는 이미 드롭 박스 계정을 가지고';
+$lang['user_dropbox_init_complete'] = '초기화 완료. 귀하의 계정에 보관 인증을 완료하기 위해 아래 링크를 클릭하십시오.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = '당신은 클라우드 기반 서비스로 로컬 서버의 폴더를 동기화하기 위해 보관 용 계정이 필요합니다.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = '지금 당신은 당신의 보관 용 계정 설정을 가지고, "지금 동기화"버튼을 클릭합니다. 당신은 당신의 홈 디렉토리에있는 드롭 박스 폴더에 키를 포함하는 URL이 제공됩니다. 그것을 클릭하고 드롭 박스 인증을 완료하여이 목적지로 보관 용 계정을 연결하려면이 링크를 사용합니다.';
+$lang['user_dropbox_invalid_account'] = '계정이 잘못되었습니다.';
+$lang['user_dropbox_not_available_for_root_account'] = '죄송 보관 서비스는 루트 계정을 사용할 수 없습니다.';
+$lang['user_dropbox_starting_account_init'] = '계정 초기화를 시작하는 중 ... 기다려주십시오.';
+$lang['user_dropbox_step_1'] = '2 단계 중 1 단계';
+$lang['user_dropbox_step_2'] = '2 단계 중 2 단계';
+$lang['user_dropbox_sync_check'] = '동기화 확인';
+$lang['user_dropbox_sync_now'] = '지금 동기화';
+$lang['user_dropbox_url_link'] = '드롭 박스 URL 링크';
+$lang['user_dropbox_waiting_confirmation'] = '드롭 박스 계정 동기화 인증을 기다리고 완료합니다. 당신은 보관 용 사이트를 방문하고 계정을 연결하지 않은 경우 아래의 드롭 박스 URL 링크를 클릭하십시오.';
