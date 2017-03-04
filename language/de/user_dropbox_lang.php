986455e1d1ad95278ac70e4044f2baa4f72f86f1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:44:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox ist eine Cloud-basierte Speicherung von Dateien und Synchronisationsdienst. Mit dieser App-Dateien in einem Ordner in Ihrem Home-Verzeichnis zu synchronisieren, die dann von jedem Gerät auf die gleiche Dropbox-Konto (Laptop, Handy, Tablet etc.) assoziiert zugegriffen werden.';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Authentifizieren mit Dropbox';
+$lang['user_dropbox_authentication'] = 'Vollständige Setup mit Dropbox durch Authentifizierung';
+$lang['user_dropbox_create_account'] = 'Erstellen Sie Dropbox-Konto';
+$lang['user_dropbox_have_account'] = 'Ich habe schon einen Dropbox Account';
+$lang['user_dropbox_init_complete'] = 'Initialisierung abgeschlossen. Bitte klicken Sie auf den Link unten Dropbox Genehmigung auf Ihr Konto zu vervollständigen.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Sie werden ein Dropbox-Konto benötigen, um Ihre lokalen Server-Ordner in den Cloud-basierten Dienst zu synchronisieren.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Nun, da Sie Ihr Dropbox-Konto eingerichtet haben, klicken Sie auf "Jetzt synchronisieren" klicken. Sie werden mit einer URL, um einen Schlüssel für den Dropbox-Ordner zur Verfügung gestellt werden, die in Ihrem Home-Verzeichnis befinden. Verwenden Sie diesen Link, um Ihre Dropbox-Konto mit diesem Ziel verknüpft werden, indem Sie darauf klicken und die Dropbox Genehmigung abgeschlossen.';
+$lang['user_dropbox_invalid_account'] = 'Konto ist ungültig.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Sorry, das Dropbox-Dienst für den Root-Account nicht zur Verfügung.';
+$lang['user_dropbox_starting_account_init'] = 'Starten Konto Initialisierung ... bitte etwas Geduld.';
+$lang['user_dropbox_step_1'] = 'Schritt 1 von 2';
+$lang['user_dropbox_step_2'] = 'Schritt 2 von 2';
+$lang['user_dropbox_sync_check'] = 'Überprüfen Sie Sync';
+$lang['user_dropbox_sync_now'] = 'Jetzt synchronisieren';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Warten auf Dropbox-Konto Synchronisation Authentifizierung abzuschließen. Bitte klicken Sie auf den Dropbox URL Link unten, wenn Sie nicht die Dropbox-Website und Ihr Konto verknüpft besucht haben.';
