7c7f4280e03efe0e3394ef499f77c657715afe94
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:42:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox on pilvi-pohjainen tiedostojen tallennukseen ja synkronointi palvelu. Käytä tätä app synkronoida tiedostoja kansioon sijaitsevat kotihakemistossasi jota voidaan sitten käyttää millä tahansa laitteella liittyvät samaan Dropbox tilille (kannettava tietokone, matkapuhelin, tabletti jne.).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Todennukseen Dropbox';
+$lang['user_dropbox_authentication'] = 'Täydellinen asennusohjelma autentikointiin Dropbox';
+$lang['user_dropbox_create_account'] = 'Luo Dropbox tilin';
+$lang['user_dropbox_have_account'] = 'Minulla on jo Dropbox tilin';
+$lang['user_dropbox_init_complete'] = 'Alustus valmis. Klikkaa alla olevaa linkkiä loppuun Dropbox lupa tilillesi.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Tarvitset Dropbox tilin synkronoida paikallisen palvelimen kansion pilvi-pohjainen palvelu.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Nyt sinulla on Dropbox tilin määrityksen, klikkaa "Synkronoi nyt" -painiketta. Sinut varustetaan URL sisältää avain Dropbox-kansioon asuvat kotihakemiston. Käytä linkki yhdistää Dropbox tilin tällä määränpää klikkaamalla sitä ja täyttämällä Dropbox lupaa.';
+$lang['user_dropbox_invalid_account'] = 'Tili on virheellinen.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Valitettavasti Dropbox palvelu ei ole käytettävissä root-tunnuksen.';
+$lang['user_dropbox_starting_account_init'] = 'Starting tili alustus ... ole kärsivällinen.';
+$lang['user_dropbox_step_1'] = 'Vaihe 1 2';
+$lang['user_dropbox_step_2'] = 'Vaihe 2 2';
+$lang['user_dropbox_sync_check'] = 'Tarkista Sync';
+$lang['user_dropbox_sync_now'] = 'synkronoida Nyt';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Odotetaan Dropbox tilin synkronointi autentikoinnin loppuun. Klikkaa Dropbox URL Link alla, jos et ole käynyt Dropbox sivuston ja yhdistänyt tilisi.';
