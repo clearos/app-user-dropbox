02e8c22cd65608b14c883d413375c9b3bedbbf48
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:35:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox és un emmagatzematge d&#39;arxius basat en el núvol i el servei de sincronització. Utilitzeu aquesta aplicació per sincronitzar arxius a una carpeta que es troba en el directori inicial que després es pot accedir mitjançant qualsevol dispositiu associat al mateix compte de Dropbox (ordinador portàtil, mòbil, tablet, etc.).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Autenticar amb Dropbox';
+$lang['user_dropbox_authentication'] = 'configuració completa mitjançant l&#39;autenticació amb Dropbox';
+$lang['user_dropbox_create_account'] = 'Crear compte de Dropbox';
+$lang['user_dropbox_have_account'] = 'Ja tinc un compte de Dropbox';
+$lang['user_dropbox_init_complete'] = 'Inicialització completa. Si us plau, feu clic a l&#39;enllaç de sota per completar l&#39;autorització al seu compte de Dropbox.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Vostè necessitarà un compte de Dropbox per sincronitzar la carpeta del servidor local per al servei basat en el núvol.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Ara que té la configuració del seu compte de Dropbox, feu clic al botó "Sincronitza ara". Se li proporcionarà amb una adreça URL que conté una clau a la carpeta de Dropbox que resideixen en el seu directori personal. Utilitzeu aquest enllaç per associar el seu compte de Dropbox amb aquesta destinació fent clic i completant l&#39;autorització de Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Compte no és vàlid.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Ho sentim, el servei Dropbox no està disponible per al compte de root.';
+$lang['user_dropbox_starting_account_init'] = 'A partir d&#39;inicialització compte ... Si us plau sigui pacient.';
+$lang['user_dropbox_step_1'] = 'Pas 1 de 2';
+$lang['user_dropbox_step_2'] = 'Pas 2 feb';
+$lang['user_dropbox_sync_check'] = 'Marqueu Sincronitzar';
+$lang['user_dropbox_sync_now'] = 'sincronitzar Ara';
+$lang['user_dropbox_url_link'] = 'Dropbox URL';
+$lang['user_dropbox_waiting_confirmation'] = 'A l&#39;espera d&#39;autenticació de compte de sincronització de Dropbox per completar. Si us plau, feu clic a l&#39;enllaç de sota Dropbox URL si no ha visitat el lloc de Dropbox i vinculat el seu compte.';
