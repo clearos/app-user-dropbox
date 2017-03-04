5f2064030ad49d0580079d641139a1977c78d98e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:33:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox je cloud pohranu datoteka i servis sinhronizacije. Koristite ovu aplikaciju za sinhronizaciju datoteka u folder nalazi u vašem domu imeniku koji se onda može pristupiti bilo koji uređaj povezan na isti Dropbox račun (laptop, mobilni, tableta i sl).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Autentičnost sa Dropbox';
+$lang['user_dropbox_authentication'] = 'Kompletna podešavanje po autentifikaciju sa Dropbox';
+$lang['user_dropbox_create_account'] = 'Napravi Dropbox račun';
+$lang['user_dropbox_have_account'] = 'Već imam Dropbox račun';
+$lang['user_dropbox_init_complete'] = 'Inicijalizacija kompletan. Molimo kliknite na link ispod da završi Dropbox ovlašćenja na svoj račun.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Trebat će vam Dropbox račun za sinhronizaciju lokalni folder server na servis u oblaku.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Sada kada imate setup svoj Dropbox nalog, kliknite na gumb "Sinkronizacija sada". Ćete dobiti URL koji sadrži ključ u folder Dropbox koji žive u vaš dom direktoriju. Koristite ovaj link da biste povezali svoj Dropbox račun u ovu destinaciju klikom na nju i dovršavajući odobrenje Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Nalog je nevažeći.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Žao nam je, Dropbox usluga nije dostupna za root račun.';
+$lang['user_dropbox_starting_account_init'] = 'Počevši račun inicijalizacije ... molim vas da budete strpljivi.';
+$lang['user_dropbox_step_1'] = 'Korak 1 od 2';
+$lang['user_dropbox_step_2'] = 'Korak 2 od 2';
+$lang['user_dropbox_sync_check'] = 'Provjerite Sync';
+$lang['user_dropbox_sync_now'] = 'Sinhronizacija sada';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Čekajući Dropbox autentifikaciju sinhronizaciju račun da biste dovršili. Molimo kliknite na Dropbox URL link ispod ako niste posjetili Dropbox stranice i povezane nalog.';
