615df204e02543938aab36ce67cdb98ecea47da6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:53:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropboxがクラウドベースのファイルストレージと同期サービスです。 その後、同じDropboxのアカウント（ラップトップ、モバイル、タブレットなど）に関連する任意のデバイスがアクセスすることができ、あなたのホームディレクトリにあるフォルダにファイルを同期するために、このアプリを使用してください。';
+$lang['user_dropbox_app_name'] = 'ドロップボックス';
+$lang['user_dropbox_authenticate_to_service'] = 'Dropboxので認証';
+$lang['user_dropbox_authentication'] = 'Dropboxので認証することで、完全なセットアップ';
+$lang['user_dropbox_create_account'] = 'Dropboxのアカウントを作成します。';
+$lang['user_dropbox_have_account'] = '私はすでにDropboxのアカウントを持っています';
+$lang['user_dropbox_init_complete'] = '初期化が完了しました。 アカウントにDropboxの認証を完了するために、下記のリンクをクリックしてください。';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'あなたは、クラウドベースのサービスに、ローカルサーバーのフォルダを同期するためのDropboxアカウントが必要になります。';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = '今、あなたはあなたのDropboxアカウントを設定していることを、「今すぐ同期」ボタンをクリックします。 あなたは自分のホームディレクトリにあるDropboxのフォルダにキーを含むURLが提供されます。 それをクリックし、Dropboxの認証を完了することによって、この先であなたのDropboxアカウントを関連付けるために、このリンクを使用します。';
+$lang['user_dropbox_invalid_account'] = 'アカウントが無効です。';
+$lang['user_dropbox_not_available_for_root_account'] = '申し訳ありませんが、Dropboxのサービスはrootアカウントでは使用できません。';
+$lang['user_dropbox_starting_account_init'] = 'アカウントの初期化を開始しています...お待ちください。';
+$lang['user_dropbox_step_1'] = '2の段階1';
+$lang['user_dropbox_step_2'] = 'ステップ2/2';
+$lang['user_dropbox_sync_check'] = 'Syncをチェック';
+$lang['user_dropbox_sync_now'] = '今すぐ同期';
+$lang['user_dropbox_url_link'] = 'DropboxのURLリンク';
+$lang['user_dropbox_waiting_confirmation'] = 'Dropboxのアカウントの同期認証の完了を待機しています。 あなたがDropboxのサイトを訪問し、あなたのアカウントをリンクしていない場合は、以下のDropboxのURLリンクをクリックしてください。';
