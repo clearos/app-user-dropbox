e5616df01e4135b4d01cb802b5088ee73b11c54a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:51:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox bụ a ígwé ojii nke dabeere na faịlụ nchekwa na mmekọrịta ọrụ. Iji nke a ngwa gakọrịta faịlụ na nchekwa dị na n&#39;ụlọ gị gị na ndekọ nke enwere ike inweta site na ngwaọrụ ọ bụla metụtara na otu Dropbox akaụntụ (laptọọpụ, mobile, mbadamba wdg).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Nyochaa na Dropbox';
+$lang['user_dropbox_authentication'] = 'Complete ntọlite ​​site authenticating na Dropbox';
+$lang['user_dropbox_create_account'] = 'Mepụta Dropbox akaụntụ';
+$lang['user_dropbox_have_account'] = 'M ugbua nwere a Dropbox akaụntụ';
+$lang['user_dropbox_init_complete'] = 'Initialization zuru ezu. Biko pịa na njikọ n&#39;okpuru iji wuchaa Dropbox ikike na akaụntụ gị.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Ị ga-mkpa a Dropbox akaụntụ mekọrịta gị na mpaghara nkesa nchekwa na igwe-oji dabeere ọrụ.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Ugbu a na i nwere gị Dropbox akaụntụ Mbido, pịa na "Mekọrịta ugbu a" button. Ị ga-nyere na a URL nwere a isi na Dropbox nchekwa bi n&#39;ụlọ gị gị ndekọ. Jiri a njikọ na-akpakọrịta gị Dropbox akaụntụ na a na-aga site na ịpị ya na-emecha Dropbox ikike.';
+$lang['user_dropbox_invalid_account'] = 'Account bụ ghara ịdị irè.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Ndo, na Dropbox ọrụ bụ maka nke mgbọrọgwụ akaụntụ.';
+$lang['user_dropbox_starting_account_init'] = 'Malite akaụntụ initialization ... biko na-enwe ndidi.';
+$lang['user_dropbox_step_1'] = 'Nzọụkwụ 1 of 2';
+$lang['user_dropbox_step_2'] = 'Nzọụkwụ 2 of 2';
+$lang['user_dropbox_sync_check'] = 'lelee mmekọrịta';
+$lang['user_dropbox_sync_now'] = 'mekọrịta Ugbu a';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = '-Eche Dropbox akaụntụ mmekọrịta Nyocha iji wuchaa. Biko pịa na Dropbox URL Link n&#39;okpuru ọ bụrụ na ị gara Dropbox saịtị ma jikọrọ na akaụntụ gị.';
