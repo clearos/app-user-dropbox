3facf7fe578fd66b98999b4a726f801d0af5e5df
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:37:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox是一个基于云的文件存储和同步服务。 使用这个应用程序的文件同步到位于你的home目录的文件夹，然后可以通过关联到同样的Dropbox帐户（笔记本，手机，平板电脑等）的设备进行访问。';
+$lang['user_dropbox_app_name'] = 'Dropbox的';
+$lang['user_dropbox_authenticate_to_service'] = '与Dropbox的验证';
+$lang['user_dropbox_authentication'] = '完整的安装与Dropbox的验证';
+$lang['user_dropbox_create_account'] = '创建的Dropbox帐户';
+$lang['user_dropbox_have_account'] = '我已经有Dropbox帐户';
+$lang['user_dropbox_init_complete'] = '初始化完成。 请点击下面的链接完成Dropbox的授权到您的帐户。';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = '您将需要一个Dropbox的帐户到本地服务器文件夹同步到基于云的服务。';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = '现在，你有你的Dropbox帐户设置，点击“立即同步”按钮。 我们将为您提供包含一键驻留在你的home目录Dropbox的文件夹中的网址。 使用此链接上按一下，并完成了Dropbox的授权你的Dropbox帐户与此目的地相关联。';
+$lang['user_dropbox_invalid_account'] = '帐号是无效的。';
+$lang['user_dropbox_not_available_for_root_account'] = '对不起，Dropbox的服务不可用root帐号。';
+$lang['user_dropbox_starting_account_init'] = '启动帐户初始化...请耐心等待。';
+$lang['user_dropbox_step_1'] = '步骤2之1';
+$lang['user_dropbox_step_2'] = '步骤2之2';
+$lang['user_dropbox_sync_check'] = '检查同步';
+$lang['user_dropbox_sync_now'] = '立即同步';
+$lang['user_dropbox_url_link'] = 'Dropbox的URL链接';
+$lang['user_dropbox_waiting_confirmation'] = '等待的Dropbox帐户同步验证完成。 请Dropbox的URL链接点击下面如果你没有访问过Dropbox的网站和链接您的帐户。';
