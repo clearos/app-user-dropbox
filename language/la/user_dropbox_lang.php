5dbfccda2024fdb8570d509996adbbaac20ae669
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:57:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dimittere est, et nubes repono synchronization file-substructio servitium. Uti app synchronise directorium quae domi posita lima ut a folder can exsisto obvius quacunque arte coniungitur Dropbox eandem rationem (laptop mobile, tabula etc).';
+$lang['user_dropbox_app_name'] = 'animadversae';
+$lang['user_dropbox_authenticate_to_service'] = 'Authenticate cum Dropbox';
+$lang['user_dropbox_authentication'] = 'The setup authenticating cum Dropbox';
+$lang['user_dropbox_create_account'] = 'Create account Dropbox';
+$lang['user_dropbox_have_account'] = 'Iam habere rationem Dropbox';
+$lang['user_dropbox_init_complete'] = 'Initialization perfecta. Click in pagina infra perficere Dropbox auctoritate vestra.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Opus erit servo Dropbox rationem loci synchronise folder nubes servitium fundatur.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Quod autem habeat rationem habetis Dropbox, click "Synchronize iam" puga pyga. Et erit cum modo domicilium habet clavem ad Dropbox apud folder in vestri domus presul. Uti ad destinatum commisceamini cum ob tua Dropbox Dropbox strepitando in perficiendis et auctoritate.';
+$lang['user_dropbox_invalid_account'] = 'Ideo invalidum est.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Paenitet, sed meritum non Dropbox propter radicali.';
+$lang['user_dropbox_starting_account_init'] = 'Satus propter initialization ... placere patientes estote.';
+$lang['user_dropbox_step_1'] = 'Step I et II';
+$lang['user_dropbox_step_2'] = 'Step II de II';
+$lang['user_dropbox_sync_check'] = 'reprehendo Sync';
+$lang['user_dropbox_sync_now'] = 'Sed synchronize';
+$lang['user_dropbox_url_link'] = 'URL dimittere Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Dropbox authenticas declarandas ratio synchronization Expectantes ad perficere. URL Dropbox click in pagina infra situm et si non visitavit Dropbox coniungitur vestra.';
