b379b67d71987d50d2a1a27178940cfc42276dfa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:55:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox គឺជាការផ្ទុកឯកសារពពកដែលមានមូលដ្ឋាននិងសេវាធ្វើសមកាលកម្ម។ ប្រើកម្មវិធីនេះដើម្បីធ្វើសមកាលកម្មឯកសារទៅថតមួយដែលមានទីតាំងក្នុងថតផ្ទះរបស់អ្នកដែលអាចត្រូវបានចូលដំណើរការដោយឧបករណ៍ណាមួយដែលបានភ្ជាប់ទៅកាន់គណនី Dropbox ដូចគ្នា (កុំព្យូទ័រយួរដៃ, ទូរស័ព្ទដៃឧបករណ៍ Tablet, ល) ។';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'ផ្ទៀងផ្ទាត់ភាពត្រឹមត្រូវជាមួយ Dropbox';
+$lang['user_dropbox_authentication'] = 'ការដំឡើងពេញលេញដោយការផ្ទៀងផ្ទាត់ជាមួយ Dropbox';
+$lang['user_dropbox_create_account'] = 'បង្កើតគណនី Dropbox';
+$lang['user_dropbox_have_account'] = 'ខ្ញុំមានគណនី Dropbox រួចទៅហើយ';
+$lang['user_dropbox_init_complete'] = 'ចាប់ផ្ដើមពេញលេញ។ សូមចុចលើតំណខាងក្រោមនេះដើម្បីបញ្ចប់ការអនុញ្ញាត Dropbox ទៅគណនីរបស់អ្នក។';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'អ្នកនឹងត្រូវការដើម្បីធ្វើសមកាលកម្មគណនី Dropbox ថតម៉ាស៊ីនបម្រើមូលដ្ឋានរបស់អ្នកទៅសេវាកម្មពពកដែលមានមូលដ្ឋាននេះ។';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'ឥឡូវអ្នកមានការដំឡើងគណនី Dropbox របស់អ្នក, សូមចុចលើប៊ូតុង "ធ្វើសមកាលកម្មឥឡូវនេះ" ។ អ្នកនឹងត្រូវបានផ្តល់ជូនជាមួយនឹង URL ដែលមានកូនសោទៅក្នុងថត Dropbox រស់នៅក្នុងថតផ្ទះរបស់អ្នកមួយ។ ប្រើតំណនេះដើម្បីភ្ជាប់គណនី Dropbox របស់អ្នកជាមួយនឹងគោលដៅនេះដោយចុចលើវានិងបំពេញការអនុញ្ញាត Dropbox ។';
+$lang['user_dropbox_invalid_account'] = 'គណនីមិនត្រឹមត្រូវ។';
+$lang['user_dropbox_not_available_for_root_account'] = 'សូមអភ័យទោស, សេវា Dropbox គឺមិនអាចប្រើសម្រាប់គណនី root ។';
+$lang['user_dropbox_starting_account_init'] = 'ការចាប់ផ្តើមការចាប់ផ្ដើមគណនី ... សូមអត់ធ្មត់។';
+$lang['user_dropbox_step_1'] = 'ជំហាន 1 នៃ 2';
+$lang['user_dropbox_step_2'] = 'ជំហានទី 2 នៃ 2';
+$lang['user_dropbox_sync_check'] = 'សូមពិនិត្យមើលការធ្វើសមកាលកម្ម';
+$lang['user_dropbox_sync_now'] = 'ធ្វើសមកាលកម្មឥឡូវនេះ';
+$lang['user_dropbox_url_link'] = 'Dropbox របស់ URL ដែលបានតំណ';
+$lang['user_dropbox_waiting_confirmation'] = 'កំពុងរង់ចាំការផ្ទៀងផ្ទាត់ភាពត្រឹមត្រូវធ្វើសមកាលកម្មគណនី Dropbox ដើម្បីបញ្ចប់។ សូមចុចលើ URL ខាងក្រោមតំណ Dropbox ប្រសិនបើអ្នកមិនបានទៅទស្សនាតំបន់បណ្តាញ Dropbox និងភ្ជាប់គណនីរបស់អ្នក។';
