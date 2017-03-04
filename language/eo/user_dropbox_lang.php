1c1250f422918c1b882ebb363f37a63d6d9ff476
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:40:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox estas nubo-bazita dosiero stokado kaj sincronización servo. Uzi tiun programon por sinkronigi dosierojn al dosierujo lokita en via hejmdosierujo kiu povas tiam esti alirita de ajna aparato asociita al la sama Dropbox konto (tekkomputilo, mobile, tablojdo ktp).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Autenticar kun Dropbox';
+$lang['user_dropbox_authentication'] = 'Kompletan aranĝo de aŭtentikigi kun Dropbox';
+$lang['user_dropbox_create_account'] = 'Krei Dropbox konto';
+$lang['user_dropbox_have_account'] = 'Mi jam havas Dropbox konto';
+$lang['user_dropbox_init_complete'] = 'Inicialización kompleta. Bonvolu alklaki la suban ligilon por kompletigi Dropbox rajtigo al via konto.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Vi bezonos Dropbox konto sinkronigi via loka servilo dosierujo al la nubo-bazita servo.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Nun ke vi havas vian Dropbox konto agordojn, klaku la "Sinkronigu nun" Butono. Vi estos provizita kun retadreso enhavanta ŝlosilo al la Dropbox dosierujo loĝante en via hejma dosierujo. Uzu tiun ligilon asocii vian Dropbox konto kun tiu destino alklakante ĝin kaj kompletigi la Dropbox rajtigo.';
+$lang['user_dropbox_invalid_account'] = 'Konto estas nevalida.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Pardonu, la Dropbox servo ne estas disponebla por la radiko konto.';
+$lang['user_dropbox_starting_account_init'] = 'Komencante konton inicialización ... bonvolu esti pacienca.';
+$lang['user_dropbox_step_1'] = 'Paŝo 1 de 2';
+$lang['user_dropbox_step_2'] = 'Paŝo 2 de 2';
+$lang['user_dropbox_sync_check'] = 'kontrolu Sync';
+$lang['user_dropbox_sync_now'] = 'sinkronigi Nun';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Atendante Dropbox konto sincronización autenticación kompletigi. Bonvolu klaki sur la Dropbox URL Link sube se vi ne vizitis la Dropbox ejo kaj ligitaj via konto.';
