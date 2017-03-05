aafc10bca67fc0f9d3938f5b1d8e3c53de5449a2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:07:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Eyinqolobane is a isitoreji ifu-based ifayela futhi isevisi yokuqondanisa. Sebenzisa lolu hlelo lokusebenza ukuba ukuvumelanisa amafayela kwifolda elise ekhaya lakho isiqondisi okungase lungatholakala nganoma iyiphi idivayisi ezihlangene akhawunti efanayo eyinqolobane (laptop, mobile, tablet njll).';
+$lang['user_dropbox_app_name'] = 'eyinqolobane';
+$lang['user_dropbox_authenticate_to_service'] = 'Qinisekisa nge eyinqolobane';
+$lang['user_dropbox_authentication'] = 'Qedela ukusethwa by yokuqinisekisa eyinqolobane';
+$lang['user_dropbox_create_account'] = 'Dala i-akhawunti eyinqolobane';
+$lang['user_dropbox_have_account'] = 'I kakade unayo i-akhawunti eyinqolobane';
+$lang['user_dropbox_init_complete'] = 'Ukuqalisa kuqedile. Sicela uchofoze isixhumanisi esingezansi ukuze ugcwalise eyinqolobane ukugunyazwa-akhawunti yakho.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Uzodinga i-akhawunti eyinqolobane ukuvumelanisa ifolda iseva wangakini isevisi ifu-based.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Manje ukuthi une yakho isethaphu akhawunti eyinqolobane, chofoza inkinobho "Vumelanisa manje". Uzonikezwa nge-URL oqukethe ukhiye enqolobaneni ifolda ohlala ekhaya lakho lwemibhalo. Sebenzisa lesi isixhumanisi ukuhlobanisa i-akhawunti yakho eyinqolobane nale uya khona ngokuchofoza kuso bese kokuqeda ukugunyazwa eyinqolobane.';
+$lang['user_dropbox_invalid_account'] = '-Akhawunti ayivumelekile.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Uxolo, isevisi eyinqolobane ayitholakali ngoba akhawunti impande.';
+$lang['user_dropbox_starting_account_init'] = 'Iqalisa akhawunti Ukuqalisa ... sicela ubekezele.';
+$lang['user_dropbox_step_1'] = 'Isinyathelo 1 of 2';
+$lang['user_dropbox_step_2'] = 'Isinyathelo 2 of 2';
+$lang['user_dropbox_sync_check'] = 'Hlola Vumelanisa';
+$lang['user_dropbox_sync_now'] = 'Vumelanisa Manje';
+$lang['user_dropbox_url_link'] = 'Eyinqolobane URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Ilinde eyinqolobane akhawunti yokuvumelanisa ubuqiniso ukuqedela. Sicela uchofoze on eyinqolobane URL Link ngezansi uma ungazange wavakashela isayithi eyinqolobane futhi uxhumanise i-akhawunti yakho.';
