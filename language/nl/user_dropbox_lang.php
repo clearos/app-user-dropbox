f1c52a6b97e3d3cb8ae4fa5f3c780fe5d53699cd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:03:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox is een cloud-gebaseerde opslag van bestanden en synchronisatie service. Gebruik deze app om bestanden te synchroniseren naar een map in je home directory die vervolgens kunnen worden geopend door een ander toestel aan dezelfde Dropbox-account (laptop, mobiel, tablet etc.).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'De echtheid van met Dropbox';
+$lang['user_dropbox_authentication'] = 'Complete opstelling van verificatie met Dropbox';
+$lang['user_dropbox_create_account'] = 'Maak een Dropbox-account';
+$lang['user_dropbox_have_account'] = 'ik heb al een Dropbox account';
+$lang['user_dropbox_init_complete'] = 'Initialisatie voltooid. Klik op de onderstaande link om Dropbox toestemming te voltooien op uw account.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'U ontvangt een Dropbox account nodig hebt om uw lokale server map te synchroniseren met de cloud-gebaseerde service.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Nu dat u uw Dropbox-account setup, klikt u op de "Synchroniseren nu" knop. U zult worden voorzien van een URL met een sleutel van de Dropbox-map die woonachtig zijn in je home directory. Gebruik deze link om je Dropbox-account koppelen aan deze bestemming door erop te klikken en het voltooien van de Dropbox vergunning.';
+$lang['user_dropbox_invalid_account'] = 'Account is ongeldig.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Sorry, de Dropbox dienst is niet beschikbaar voor het root-account.';
+$lang['user_dropbox_starting_account_init'] = 'Vanaf rekening initialisatie ... even geduld.';
+$lang['user_dropbox_step_1'] = 'Stap 1 van 2';
+$lang['user_dropbox_step_2'] = 'Stap 2 van 2';
+$lang['user_dropbox_sync_check'] = 'controleer Sync';
+$lang['user_dropbox_sync_now'] = 'Synchroniseer Nu';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Wachten op Dropbox-account synchronisatie verificatie te voltooien. Klik op de Dropbox URL Link hieronder als u niet de Dropbox website hebben bezocht en gelinkt uw account.';
