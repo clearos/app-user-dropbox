45e15d6d34ae79b6bc445edc546358a1533f708f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:01:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox үүл дээр тулгуурласан файлын хадгалах, синхрончлол үйлчилгээ юм. таны гэрийн санд байрлах хавтас дараа нь мөн Dropbox-ын дансанд (зөөврийн компьютер, гар утас, таблет гэх мэт) холбоотой аливаа төхөөрөмж хандаж болно файлыг хамгийн сүүлийн хэлбэрт аваачиж энэ програмыг ашиглах хэрэгтэй.';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Dropbox нь таниулан';
+$lang['user_dropbox_authentication'] = 'Dropbox нь таньж хамт иж бүрэн тохиргоо';
+$lang['user_dropbox_create_account'] = 'Dropbox Бүртгэл үүсгэх';
+$lang['user_dropbox_have_account'] = 'Би аль хэдийн Dropbox-ын данстай байх';
+$lang['user_dropbox_init_complete'] = 'Эхлүүлэлт нь бүрэн. таны дансанд Dropbox зөвшөөрөл дуусгахын тулд доорх холбоос дээр дарна уу.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Та үүл дээр суурилсан үйлчилгээ нь таны локал серверийн хавтсыг хамгийн сүүлийн хэлбэрт аваачиж нь Dropbox данс хэрэгтэй болно.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Одоо та өөрийн Dropbox дансны тохиргоог байна гэж, "Одоо синхрончлох" товч дээр дарна уу. Та Dropbox хавтас таны гэрийн санд оршин сууж байгаа нь түлхүүрийг агуулсан URL олгоно. Хэрэв дээр дарж, Dropbox зөвшөөрөл дуусгаж, энэ очих нь таны Dropbox данс холбогдож энэ холбоосыг ашиглаарай.';
+$lang['user_dropbox_invalid_account'] = 'Дансны хүчин төгөлдөр бус байна.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Уучлаарай, Dropbox үйлчилгээний үндэс данс байхгүй байна.';
+$lang['user_dropbox_starting_account_init'] = 'данс эхлүүлэлтийг эхлэн ... тэвчээртэй байх уу.';
+$lang['user_dropbox_step_1'] = '2 Алхам 1';
+$lang['user_dropbox_step_2'] = '2-ын 2-р алхам';
+$lang['user_dropbox_sync_check'] = 'Sync шалгах';
+$lang['user_dropbox_sync_now'] = 'Одоо хамгийн сүүлийн хэлбэрт аваачиж';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'дуусгахын тулд Dropbox данс синхрончлол танилтад зориулсан хүлээж байна. Та Dropbox сайтад очоогүй байгаа бол доорх болон таны дансыг холбоотой Dropbox URL линк дээр дарна уу.';
