e31d6baa19dc64aa5708108c5c0f69b7c3bc1529
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:06:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox bulut tabanlı dosya depolama ve senkronizasyon hizmetidir. daha sonra aynı Dropbox hesabı (vb dizüstü bilgisayar, cep telefonu, tablet) ilişkili herhangi bir cihaz ile ulaşılabilir ev dizininde bulunan bir klasöre dosya eşitlemek için bu uygulamayı kullanın.';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Dropbox ile kimlik doğrulaması';
+$lang['user_dropbox_authentication'] = 'Dropbox ile kimlik doğrulaması Komple kurulum';
+$lang['user_dropbox_create_account'] = 'Dropbox hesabı oluştur';
+$lang['user_dropbox_have_account'] = 'Zaten bir Dropbox hesabınız var';
+$lang['user_dropbox_init_complete'] = 'Başlatma tamamlandı. Hesabınıza Dropbox yetki tamamlamak için aşağıdaki linke tıklayınız.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Bulut tabanlı hizmete yerel sunucu klasörü eşitlemek için bir Dropbox hesabı gerekir.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Şimdi Dropbox hesap kurulumunu olduğunu, "Şimdi senkronize etme" butonuna tıklayın. Evinizde dizininde bulunan Dropbox klasörüne anahtarını içeren bir URL ile sağlanacaktır. üzerine tıklayarak ve Dropbox yetki tamamlayarak bu hedef ile Dropbox hesabı ilişkilendirmek için bu bağlantıyı kullanın.';
+$lang['user_dropbox_invalid_account'] = 'Hesap geçersiz.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Maalesef, Dropbox hizmet root hesabı için geçerli değildir.';
+$lang['user_dropbox_starting_account_init'] = 'Hesap başlatma başlatılıyor ... lütfen sabırlı olun.';
+$lang['user_dropbox_step_1'] = '1. Adım 2';
+$lang['user_dropbox_step_2'] = '2 Aşama 2';
+$lang['user_dropbox_sync_check'] = 'Sync kontrol';
+$lang['user_dropbox_sync_now'] = 'Şimdi senkronize';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Bağlantısı';
+$lang['user_dropbox_waiting_confirmation'] = 'Dropbox hesabı eşitleme kimlik doğrulaması için bekliyorum tamamlayın. Dropbox sitesini ziyaret ve hesabınızı bağlantılı değil varsa aşağıda Dropbox URL Bağlantı tıklayınız.';
