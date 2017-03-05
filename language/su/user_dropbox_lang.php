563437eece40f03caba859c68261e5380fcbcc1b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:05:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox mangrupa gudang file basis awan jeung layanan sinkronisasi. Paké aplikasi ieu pikeun nyinkronkeun payil ka polder nu aya di diréktori imah Anjeun nu bisa terus diakses ku nu mana wae alat patali ka akun Dropbox sami (laptop, mobile, tablet jsb).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Ngabuktoskeun kaaslianana jeung Dropbox';
+$lang['user_dropbox_authentication'] = 'Lengkep setelan ku authenticating jeung Dropbox';
+$lang['user_dropbox_create_account'] = 'Jieun akun Dropbox';
+$lang['user_dropbox_have_account'] = 'Kuring geus boga akun Dropbox';
+$lang['user_dropbox_init_complete'] = 'Initialization lengkep. Mangga klik dina link di handap pikeun ngarengsekeun otorisasina Dropbox ka akun Anjeun.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Anjeun bakal butuh hiji akun Dropbox pikeun nyinkronkeun polder server lokal Anjeun ka ladenan basis awan.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Ayeuna yen Anjeun gaduh setelan akun Dropbox Anjeun, klik dina "Singkronkeun ayeuna" tombol. Anjeun bakal disadiakeun ku URL ngandung konci ka Dropbox polder residing di diréktori imah Anjeun. Paké link ieu ngahubungkeun akun Dropbox Anjeun sareng tujuan ieu ku ngaklik dina eta jeung completing otorisasina Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Akun anu sah.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Hampura, ladenan Dropbox teu sadia pikeun akun akar.';
+$lang['user_dropbox_starting_account_init'] = 'Dimimitian initialization akun ... mangga jadi sabar.';
+$lang['user_dropbox_step_1'] = 'Lengkah 1 of 2';
+$lang['user_dropbox_step_2'] = 'Lengkah 2 2';
+$lang['user_dropbox_sync_check'] = 'pariksa Sync';
+$lang['user_dropbox_sync_now'] = 'nyingkronkeun Ayeuna';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Nungguan Dropbox auténtikasi sinkronisasi akun pikeun ngarengsekeun. Mangga klik dina Dropbox URL Link di handap lamun geus teu dilongok situs Dropbox jeung numbu akun Anjeun.';
