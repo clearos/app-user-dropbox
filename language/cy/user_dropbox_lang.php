391a60ebd8e2933c880a43c7d8cd45aecfccf8ac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:07:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox yn storio ffeiliau sy&#39;n seiliedig ar cwmwl-a gwasanaeth synchronization. Defnyddiwch y app hwn i gydamseru ffeiliau i&#39;r ffolder lleoli yn eich cyfeiriadur cartref y gellir wedyn eu defnyddio gan unrhyw ddyfais sy&#39;n gysylltiedig â&#39;r un cyfrif Dropbox (gliniadur, ffôn symudol, tabled ac ati).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Dilysu gyda Dropbox';
+$lang['user_dropbox_authentication'] = 'setup gwblhau erbyn dilysu gyda Dropbox';
+$lang['user_dropbox_create_account'] = 'Creu cyfrif Dropbox';
+$lang['user_dropbox_have_account'] = 'Rwyf eisoes gennych gyfrif Dropbox';
+$lang['user_dropbox_init_complete'] = 'Initialization cyflawn. Cliciwch ar y ddolen isod i gwblhau awdurdodiad Dropbox at eich cyfrif.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Bydd angen cyfrif Dropbox i chi i gydamseru eich ffolder gweinyddwr lleol i&#39;r gwasanaeth sy&#39;n seiliedig cwmwl.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Nawr eich bod wedi eich setup cyfrif Dropbox, cliciwch ar y botwm "Cydamseru Now". Byddwch yn derbyn URL yn cynnwys allwedd i&#39;r ffolder Dropbox sy&#39;n byw yn eich cyfeiriadur cartref. Defnyddiwch y ddolen hon i gysylltu eich cyfrif Dropbox gyda gyrchfan hon drwy glicio arno a chwblhau&#39;r awdurdodiad Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Cyfrif yn annilys.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Mae&#39;n ddrwg gennym, nid yw&#39;r gwasanaeth Dropbox ar gael ar gyfer y cyfrif gwraidd.';
+$lang['user_dropbox_starting_account_init'] = 'Dechrau cyfrif initialization ... byddwch yn amyneddgar.';
+$lang['user_dropbox_step_1'] = 'Cam 1 o 2';
+$lang['user_dropbox_step_2'] = 'Cam 2 o 2';
+$lang['user_dropbox_sync_check'] = 'gwirio Sync';
+$lang['user_dropbox_sync_now'] = 'cydamseru Now';
+$lang['user_dropbox_url_link'] = 'Dropbox Dolen URL';
+$lang['user_dropbox_waiting_confirmation'] = 'Aros am Dropbox dilysu cyfrif synchronization i&#39;w gwblhau. Cliciwch ar y Dropbox URL Link isod os nad ydych wedi ymweld â&#39;r safle Dropbox ac yn cysylltu eich cyfrif.';
