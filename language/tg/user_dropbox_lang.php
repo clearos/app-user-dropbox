0f50c5d42d6774db65f5b73be7f54102edbc30d8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:06:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox нигоҳдорӣ файли дар асоси абр ва хадамоти ҳамоҳангсозӣ аст. Истифодаи ин барнома ба ҳамоҳангсозии файлҳо ба ягон ҷузвдон воқеъ дар феҳристи хонагӣ худ, ки баъдан метавонанд аз ҷониби дастгохи дилхоҳ алоқаманд ба ҳисоби Dropbox ҳамон (ноутбук мобилӣ, планшет ва ғайра) дастрас карда мешавад.';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Аслӣ дар Dropbox';
+$lang['user_dropbox_authentication'] = 'сабти пурра аз тарафи authenticating бо Dropbox';
+$lang['user_dropbox_create_account'] = 'Эҷоди ҳисоби Dropbox';
+$lang['user_dropbox_have_account'] = 'Ман аллакай ҳисоби Dropbox доранд';
+$lang['user_dropbox_init_complete'] = 'Initialization пурра. Лутфан, дар бораи пайванди дар поён пахш кунед ба анҷом иҷозати Dropbox ба ҳисоби шумо.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Шумо як ҳисоби Dropbox ба ҳамоҳангсозии феҳристи сервер маҳаллиатон ба хизмати асоси абр лозим аст.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Акнун, ки шумо ҳисоби Dropbox худ, оид ба «ҳамоҳанг Акнун» -ро пахш намоед. Шумо бо як URL дорои калиди ба папкаи Dropbox муқими феҳристи хона таъмин карда мешавад. Истифода аз ин пайванд муошират ҳисоби Dropbox худро бо ин таъинот ба воситаи ангуштзанӣ ба он ва ба итмом расонидани иҷозатномаи Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Ҳисоби нодуруст аст.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Мутаасифона, хадамоти Dropbox аст, ки барои ҳисоби реша дастрас нест.';
+$lang['user_dropbox_starting_account_init'] = 'Оғози initialization ҳисоби ... лутфан сабр кун.';
+$lang['user_dropbox_step_1'] = 'Қадами 1 аз 2';
+$lang['user_dropbox_step_2'] = 'Қадами 2 аз 2';
+$lang['user_dropbox_sync_check'] = 'санҷед, Ҳмҳнг';
+$lang['user_dropbox_sync_now'] = 'Акнун ҳамоҳанг';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Интизорӣ барои Dropbox аслӣ синхронизатсияи ҳисоби ба анҷом. Лутфан оид ба Dropbox URL Link пахш поён, агар шумо дар сомона Dropbox кардаанд, дидан нест ва ҳисоби худ вобастагии зиёд дорад.';
