6cbb00612b9a2d7e6700353ff0e94fa20361be28
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:02:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox huwa sħaba bbażata fuq fajl ħażna u servizz sinkronizzazzjoni. Uża dan app biex jissinkronizzaw fajls għal folder li jinsabu fid-direttorju dar tiegħek li mbagħad jistgħu jiġu aċċessati minn kwalunkwe mezz assoċjat mal-istess kont Dropbox (laptop, mobbli, pillola, eċċ).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Jawtentikaw ma Dropbox';
+$lang['user_dropbox_authentication'] = 'setup kompluta jiġu awtentifikati bil Dropbox';
+$lang['user_dropbox_create_account'] = 'Oħloq kont Dropbox';
+$lang['user_dropbox_have_account'] = 'I diġà għandhom kont Dropbox';
+$lang['user_dropbox_init_complete'] = 'Inizjalizzazzjoni kompluta. Jekk jogħġbok ikklikkja fuq il-link t&#39;hawn taħt biex jitlesta awtorizzazzjoni Dropbox għall-kont tiegħek.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Ikollok bżonn ta &#39;kont Dropbox synchronize folder tiegħek server lokali għas-servizz sħaba bbażata fuq.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Issa li inti għandek tiegħek setup kont Dropbox, ikklikkja fuq il- "Synchronize issa" buttuna. Inti ser tkun ipprovduta bil-URL fih hemm ċavetta għall-folder Dropbox residenti direttorju dar tiegħek. Uża din ir-rabta biex jassoċjaw kont Dropbox tiegħek ma &#39;din id-destinazzjoni billi tikklikkja fuqha u tikkompleta l-awtorizzazzjoni Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Kont huwa invalidu.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Jiddispjacini, is-servizz Dropbox ma tkunx disponibbli għall-kont għerq.';
+$lang['user_dropbox_starting_account_init'] = 'Bidu kont inizjalizzazzjoni ... jekk jogħġbok kun pazjent.';
+$lang['user_dropbox_step_1'] = 'Stadju 1 minn 2';
+$lang['user_dropbox_step_2'] = 'Stadju 2 minn 2';
+$lang['user_dropbox_sync_check'] = 'Iċċekkja sync';
+$lang['user_dropbox_sync_now'] = 'synchronize Issa';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Stennija għall Dropbox awtentikazzjoni kont sinkronizzazzjoni biex jitlesta. Jekk jogħġbok ikklikkja fuq il-URL Link Dropbox hawn taħt jekk int ma żar is-sit Dropbox u marbuta kont tiegħek.';
