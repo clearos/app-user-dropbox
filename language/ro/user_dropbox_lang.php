179a84d746c82f6651bb3f62321d5edb7bb6f9dd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:04:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox este un fișier de stocare cloud și servicii de sincronizare. Utilizați această aplicație pentru a sincroniza fișiere într-un dosar aflat în directorul acasă, care pot fi apoi accesate de către orice dispozitiv asociat cu același cont Dropbox (laptop, telefon mobil, tabletă etc.).';
+$lang['user_dropbox_app_name'] = 'dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Autentificarea cu Dropbox';
+$lang['user_dropbox_authentication'] = 'Configurarea completă cu autentificării Dropbox';
+$lang['user_dropbox_create_account'] = 'Creați un cont Dropbox';
+$lang['user_dropbox_have_account'] = 'Am deja un cont Dropbox';
+$lang['user_dropbox_init_complete'] = 'Inițializarea completă. Vă rugăm să faceți clic pe link-ul de mai jos pentru a finaliza autorizarea Dropbox în cont.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Veți avea nevoie de un cont Dropbox pentru a sincroniza folderul serverul local pentru serviciul cloud.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Acum, că aveți de configurare a contului Dropbox, faceți clic pe butonul "Sincronizare acum". Vei fi prevăzut cu o adresă URL care conține o cheie în folderul Dropbox care are reședința în directorul dvs. acasă. Utilizați acest link pentru a asocia contul Dropbox cu această destinație, făcând clic pe ea și completarea autorizației Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Contul este nevalid.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Ne pare rău, serviciul Dropbox nu este disponibil pentru contul de root.';
+$lang['user_dropbox_starting_account_init'] = 'Contul de pornire de inițializare ... vă rugăm să aveți răbdare.';
+$lang['user_dropbox_step_1'] = 'Etapa 1 din 2';
+$lang['user_dropbox_step_2'] = 'Etapa 2 din 2';
+$lang['user_dropbox_sync_check'] = 'Sincronizare verificați';
+$lang['user_dropbox_sync_now'] = 'Sincronizare acum';
+$lang['user_dropbox_url_link'] = 'Dropbox URL-Link';
+$lang['user_dropbox_waiting_confirmation'] = 'În așteptare pentru Dropbox autentificare sincronizare cont pentru a finaliza. Vă rugăm să faceți clic pe Dropbox URL-ul de mai jos dacă Link nu ați vizitat site-ul Dropbox și conectat contul.';
