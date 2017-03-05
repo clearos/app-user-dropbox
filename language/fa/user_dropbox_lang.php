dc7a0daf78e637e67c4b9ffac82748ce5fab6f9a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:03:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox به ذخیره سازی فایل مبتنی بر ابر و خدمات همگام سازی است. استفاده از این برنامه برای همگام سازی فایل را به یک پوشه واقع در پوشه خانگی خود را که پس از آن می تواند توسط هر دستگاه را در ارتباط به حساب Dropbox همان (لپ تاپ، تلفن همراه، تبلت و غیره) قابل دسترسی است.';
+$lang['user_dropbox_app_name'] = 'Dropbox به';
+$lang['user_dropbox_authenticate_to_service'] = 'تأیید اعتبار با Dropbox به';
+$lang['user_dropbox_authentication'] = 'راه اندازی کامل توسط تصدیق هویت با Dropbox';
+$lang['user_dropbox_create_account'] = 'ایجاد حساب کاربری Dropbox به';
+$lang['user_dropbox_have_account'] = 'من در حال حاضر یک حساب Dropbox';
+$lang['user_dropbox_init_complete'] = 'مقدار دهی اولیه کامل. لطفا بر روی لینک زیر کلیک کنید برای تکمیل مجوز Dropbox به حساب شما.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'شما یک حساب Dropbox برای همگام سازی پوشه سرور محلی خود را به سرویس مبتنی بر ابر نیاز دارید.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'حالا که شما راه اندازی حساب Dropbox خود، بر روی "همگام سازی" را فشار دهید کلیک کنید. شما می توانید با یک URL حاوی یک کلید به پوشه Dropbox ساکن در دایرکتوری خانگی خود را ارائه. با استفاده از این لینک برای ارتباط با حساب Dropbox خود را با این مقصد را با کلیک کردن بر روی آن و تکمیل مجوز Dropbox به است.';
+$lang['user_dropbox_invalid_account'] = 'حساب نامعتبر است.';
+$lang['user_dropbox_not_available_for_root_account'] = 'با عرض پوزش، سرویس Dropbox به است برای حساب ریشه در دسترس نیست.';
+$lang['user_dropbox_starting_account_init'] = 'شروع اولیه حساب ... لطفا صبور باشید.';
+$lang['user_dropbox_step_1'] = 'مرحله 1 از 2';
+$lang['user_dropbox_step_2'] = 'مرحله 2 از 2';
+$lang['user_dropbox_sync_check'] = 'بررسی همگام سازی';
+$lang['user_dropbox_sync_now'] = 'همگام سازی در حال حاضر';
+$lang['user_dropbox_url_link'] = 'Dropbox به URL لینک';
+$lang['user_dropbox_waiting_confirmation'] = 'در حال انتظار برای Dropbox به احراز هویت هماهنگ سازی حساب کاربری برای تکمیل. لطفا در Dropbox به URL لینک زیر کلیک کنید اگر شما در سایت Dropbox به بازدید و حساب شما مرتبط است.';
