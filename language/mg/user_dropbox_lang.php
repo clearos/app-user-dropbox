5006ce691ef48fb49dc426509dacaf509fd4d0d8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:00:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox dia rahona-monina rakitra fitehirizana sy ny synchronization fanompoana. Ampiasao izany mba synchronize fampiharana ny fampirimana antontan-taratasy ho hita ao an-tokantranonao lahatahiry izay afaka dia azo jerena misy fitaovana mifandray amin&#39;ny Dropbox ihany kaonty (solosaina, finday, takelaka etc.).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Manamarina amin&#39;ny Dropbox';
+$lang['user_dropbox_authentication'] = 'Complete setup amin&#39;ny alalan&#39;ny fanamarinana tsy azo lavina amin&#39;ny Dropbox';
+$lang['user_dropbox_create_account'] = 'Hamorona kaonty Dropbox';
+$lang['user_dropbox_have_account'] = 'Izaho efa manana Dropbox kaonty';
+$lang['user_dropbox_init_complete'] = 'Initialization tanteraka. Azafady tsindrio eo ny rohy eto ambany hamitana Dropbox alàlana ho voninahitrareo aho.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Dia mila ny Dropbox synchronize kaonty mpizara ny eo an-toerana fampirimana ny rahona-monina ny fanompoana.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Izao fa manana ny Dropbox setup kaonty, tsindrio eo amin&#39;ny "Synchronize izao" bokotra. Ho omena amin&#39;ny URL misy ny fanalahidy ho amin&#39;ny Dropbox fampirimana monina ao an-tranonao lahatahiry. Ampiasao ity rohy ity mba nifanerasera ny Dropbox kaonty amin&#39;ny toerana izany amin&#39;ny fipihana eo ka vita ny Dropbox dàlana.';
+$lang['user_dropbox_invalid_account'] = 'Account dia tsy manan-kery.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Miala tsiny, ny Dropbox fanompoana dia tsy azo ny fakany kaonty.';
+$lang['user_dropbox_starting_account_init'] = 'Manomboka kaonty initialization ... azafady hanam-paharetana.';
+$lang['user_dropbox_step_1'] = 'Dingana 1 amin&#39;ny 2';
+$lang['user_dropbox_step_2'] = 'Dingana 2 amin&#39;ny 2';
+$lang['user_dropbox_sync_check'] = 'Check Sync';
+$lang['user_dropbox_sync_now'] = 'Synchronize Ankehitriny';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Miandry Dropbox kaonty synchronization fanamarinana vao vita. Azafady tsindrio eo amin&#39;ny Dropbox URL Link eto ambany raha tsy nitsidika ny Dropbox toerana sy nampifandray ny tantara.';
---
+++ language/mi/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox Ko te rokiroki kōnae e hāngai ana-kapua, me te mahi tukutahi. Whakamahia tēnei taupānga ki te tukutahi i ngā kōnae ki te kōpaki kei roto i tou whaiaronga kāinga taea reira e te uru e tetahi pūrere e pā ana ki te pūkete Dropbox taua (pona, pūkoro, papa etc).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Whakamotuhēhē ki Dropbox';
+$lang['user_dropbox_authentication'] = 'Whakaotia tatūnga mā te motuhēhē ana me Dropbox';
+$lang['user_dropbox_create_account'] = 'Waihanga pūkete Dropbox';
+$lang['user_dropbox_have_account'] = 'E kua ahau he pūkete Dropbox';
+$lang['user_dropbox_init_complete'] = 'Arawhiti oti. Tēnā pāwhiri i runga i te hono i raro nei ki te whakaoti i Dropbox fakamafai&#39;i ki tō pūkete.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Ka Me koe he pūkete Dropbox ki te tukutahi tō kōpaki tūmau rohe ki te mahi e hāngai ana-kapua.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Na e whai koe tou tatū pūkete Dropbox, pāwhiri i runga i te "Tukutahi Na" pātene. kia koe e whakaratohia ki te URL kei roto i te kī ki te kōpaki Dropbox noho i roto i tō whaiaronga kāinga. Whakamahia tēnei hono ki te pāhono tō pūkete Dropbox ki tenei ūnga mā te pāwhiri i runga taua mea, me te whakaoti i te whakamanatanga Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'He muhu Pūkete.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Aroha mai, e kore te mea e wātea ana hoki te pūkete pakiaka te ratonga Dropbox.';
+$lang['user_dropbox_starting_account_init'] = 'Tīmata arawhiti pūkete ... tēnā kia manawanui.';
+$lang['user_dropbox_step_1'] = 'Hipanga 1 o 2';
+$lang['user_dropbox_step_2'] = 'Hipanga 2 o 2';
+$lang['user_dropbox_sync_check'] = 'Tirohia Tukutahi';
+$lang['user_dropbox_sync_now'] = 'tukutahi Na';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Hononga';
+$lang['user_dropbox_waiting_confirmation'] = 'Tatari hoki Dropbox tukutahitanga pūkete motuhēhēnga ki te whakaoti. Tēnā pāwhiri i runga i te Dropbox URL Link raro ki te kahore i koe tae te pae Dropbox, a hono tō pūkete.';
