75ae0958beebd011ae41086204fac2a16fb6de4e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:03:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox ਇੱਕ ਬੱਦਲ-ਅਧਾਰਿਤ ਫਾਇਲ ਸਟੋਰੇਜ਼ ਅਤੇ ਸਮਕਾਲੀ ਸੇਵਾ ਹੈ. ਜੋ ਫਿਰ ਉਸੇ ਹੀ ਡ੍ਰੌਪਬਾਕਸ ਖਾਤੇ (ਲੈਪਟਾਪ, ਮੋਬਾਈਲ, ਟੈਬਲੇਟ ਆਦਿ) ਨੂੰ ਸਬੰਧਤ ਕਿਸੇ ਵੀ ਜੰਤਰ ਨੂੰ ਦੇ ਕੇ ਇਸਤੇਮਾਲ ਕੀਤਾ ਜਾ ਸਕਦਾ ਹੈ, ਤੁਹਾਡੇ ਘਰ ਵਿੱਚ ਮੌਜੂਦ ਹੁੰਦੀ ਹੈ, ਇੱਕ ਫੋਲਡਰ ਨੂੰ ਫਾਇਲ ਨੂੰ ਸਮਕਾਲੀ ਕਰਨ ਲਈ ਇਸ ਐਪਲੀਕੇਸ਼ ਨੂੰ ਵਰਤੋ.';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Dropbox ਨਾਲ ਪ੍ਰਮਾਣਿਤ';
+$lang['user_dropbox_authentication'] = 'Dropbox ਨਾਲ ਪ੍ਰਮਾਣਿਤ ਕੇ ਮੁਕੰਮਲ ਸੈੱਟਅੱਪ';
+$lang['user_dropbox_create_account'] = 'Dropbox ਖਾਤਾ ਬਣਾਓ';
+$lang['user_dropbox_have_account'] = 'ਮੈਨੂੰ ਹੀ ਇੱਕ ਖਾਤਾ ਹੈ ਡ੍ਰੌਪਬਾਕਸ';
+$lang['user_dropbox_init_complete'] = 'ਸ਼ੁਰੂਆਤ ਮੁਕੰਮਲ ਹੋ. ਆਪਣੇ ਖਾਤੇ ਨੂੰ Dropbox ਅਧਿਕਾਰ ਨੂੰ ਪੂਰਾ ਕਰਨ ਲਈ ਹੇਠ ਦਿੱਤੇ ਲਿੰਕ ਤੇ ਕਲਿੱਕ ਕਰੋ ਜੀ.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'ਤੁਹਾਨੂੰ ਬੱਦਲ-ਅਧਾਰਿਤ ਸੇਵਾ ਕਰਨ ਲਈ ਆਪਣੇ ਸਥਾਨਕ ਸਰਵਰ ਫੋਲਡਰ ਨੂੰ ਸਮਕਾਲੀ ਕਰਨ ਲਈ ਇੱਕ ਡ੍ਰੌਪਬਾਕਸ ਖਾਤੇ ਦੀ ਲੋੜ ਹੋਵੇਗੀ.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'ਹੁਣ ਤੁਹਾਨੂੰ ਆਪਣੇ Dropbox ਖਾਤੇ ਸੈੱਟਅੱਪ ਹੈ, ਜੋ ਕਿ, "ਹੁਣ ਸਮਕਾਲੀ" ਬਟਨ &#39;ਤੇ ਕਲਿੱਕ ਕਰੋ. ਤੁਹਾਨੂੰ ਇੱਕ URL ਨੂੰ ਤੁਹਾਡੀ ਘਰ ਡਾਇਰੈਕਟਰੀ ਵਿੱਚ ਰਹਿ ਡ੍ਰੌਪਬਾਕਸ ਫੋਲਡਰ ਵਿੱਚ ਇੱਕ ਕੁੰਜੀ ਨੂੰ ਰੱਖਣ ਨਾਲ ਮੁਹੱਈਆ ਕੀਤਾ ਜਾਵੇਗਾ. ਇਸ &#39;ਤੇ ਕਲਿੱਕ ਕਰਕੇ ਅਤੇ Dropbox ਅਧਿਕਾਰ ਨੂੰ ਪੂਰਾ ਕਰ ਕੇ ਇਸ ਨੂੰ ਮੰਜ਼ਿਲ ਦੇ ਨਾਲ ਆਪਣੇ Dropbox ਖਾਤੇ ਸਬੰਧਤ ਕਰਨ ਲਈ ਇਸ ਲਿੰਕ ਨੂੰ ਵਰਤੋ.';
+$lang['user_dropbox_invalid_account'] = 'ਖਾਤਾ ਗਲਤ ਹੈ.';
+$lang['user_dropbox_not_available_for_root_account'] = 'ਮੁਆਫ ਕਰਨਾ, Dropbox ਸੇਵਾ ਰੂਟ ਖਾਤੇ ਲਈ ਉਪਲੱਬਧ ਨਹੀ ਹੈ.';
+$lang['user_dropbox_starting_account_init'] = 'ਖਾਤੇ ਸ਼ੁਰੂ ਸ਼ੁਰੂ ਹੋ ਰਿਹਾ ਹੈ ... ਕਿਰਪਾ ਕਰਕੇ ਮਰੀਜ਼ ਹੋ.';
+$lang['user_dropbox_step_1'] = '2 ਦੇ ਕਦਮ 1';
+$lang['user_dropbox_step_2'] = '2 ਦੇ ਕਦਮ 2';
+$lang['user_dropbox_sync_check'] = 'ਸਿੰਕ ਚੈੱਕ ਕਰੋ';
+$lang['user_dropbox_sync_now'] = 'ਹੁਣ ਸਮਕਾਲੀ';
+$lang['user_dropbox_url_link'] = 'Dropbox URL ਨੂੰ ਲਿੰਕ';
+$lang['user_dropbox_waiting_confirmation'] = 'Dropbox ਖਾਤੇ ਸਮਕਾਲੀ ਪ੍ਰਮਾਣਿਕਤਾ ਲਈ ਉਡੀਕ ਨੂੰ ਪੂਰਾ ਕਰਨ ਲਈ. ਹੇਠ ਜੇਕਰ ਤੁਹਾਨੂੰ Dropbox ਸਾਈਟ ਦਾ ਦੌਰਾ ਨਹੀ ਕੀਤਾ ਹੈ ਅਤੇ ਤੁਹਾਡੇ ਖਾਤੇ ਨਾਲ ਲਿੰਕ &#39;ਤੇ ਡ੍ਰੌਪਬਾਕਸ URL ਨੂੰ ਲਿੰਕ ਨੂੰ ਕਲਿੱਕ ਕਰੋ ਜੀ.';
