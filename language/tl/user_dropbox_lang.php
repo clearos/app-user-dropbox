35c7c4f7cb7b3f429cdce0cd77bdcc857866c438
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:06:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox ay isang ulap-based na imbakan ng file at pag-synchronize ng serbisyo. Gamitin ang app na ito upang i-synchronize ang mga file sa isang folder na matatagpuan sa iyong tahanan na direktoryo na maaaring pagkatapos ma-access ng anumang aparato na nauugnay sa parehong Dropbox account (laptop, mobile, tablet at iba pa).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Patunayan na may Dropbox';
+$lang['user_dropbox_authentication'] = 'Complete setup sa pamamagitan ng pagpapatunay na may Dropbox';
+$lang['user_dropbox_create_account'] = 'Lumikha Dropbox account';
+$lang['user_dropbox_have_account'] = 'Mayroon na akong isang Dropbox account';
+$lang['user_dropbox_init_complete'] = 'Initialization kumpleto. Paki-klik ang link sa ibaba upang makumpleto Dropbox authorization sa iyong account.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Kakailanganin mo ng isang Dropbox account upang i-synchronize ang iyong mga lokal na folder server sa serbisyo cloud-based.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Ngayon na mayroon kang ang iyong setup Dropbox account, mag-click sa "I-synchronize Now" button. Ikaw ay ibinigay sa isang URL na naglalaman ng isang susi sa Dropbox folder na nakatira sa iyong tahanan na direktoryo. Gamitin ang link na ito upang iugnay ang iyong Dropbox account gamit ang destinasyon sa pamamagitan ng pag-click dito at pagkumpleto ng Dropbox pag-debit.';
+$lang['user_dropbox_invalid_account'] = 'Account ay hindi wasto.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Paumanhin, ang Dropbox serbisyo ay hindi magagamit para sa root account.';
+$lang['user_dropbox_starting_account_init'] = 'Simula account initialization ... mangyaring maging mahinahon.';
+$lang['user_dropbox_step_1'] = 'Hakbang 1 ng 2';
+$lang['user_dropbox_step_2'] = 'Hakbang 2 ng 2';
+$lang['user_dropbox_sync_check'] = 'Tingnan Sync';
+$lang['user_dropbox_sync_now'] = 'I-synchronize Ngayon';
+$lang['user_dropbox_url_link'] = 'Dropbox Link URL';
+$lang['user_dropbox_waiting_confirmation'] = 'Naghihintay para sa Dropbox account synchronization authentication upang makumpleto. Paki-klik ang Dropbox URL Link sa ibaba kung hindi mo pa binisita ang Dropbox site at naka-link ang iyong mga account.';
