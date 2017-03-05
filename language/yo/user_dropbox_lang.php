dccea45d4d3791fe77625b8819c7d180f8b84d54
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:07:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox jẹ a awọsanma-orisun faili ipamọ ati mimuuṣiṣẹpọ iṣẹ. Lo yi app lati muu awọn faili si folda kan be ni ile rẹ liana eyi ti o le ki o si wa ni wọle nipa eyikeyi ẹrọ ni nkan si kanna Dropbox iroyin (laptop, mobile, tabulẹti ati be be lo).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Fi jeri pelu Dropbox';
+$lang['user_dropbox_authentication'] = 'Pipe setup nipa Fidi pẹlu Dropbox';
+$lang['user_dropbox_create_account'] = 'Ṣẹda Dropbox iroyin';
+$lang['user_dropbox_have_account'] = 'Mo tẹlẹ ni a Dropbox iroyin';
+$lang['user_dropbox_init_complete'] = 'Initialization pari. Jọwọ tẹ lori awọn ọna asopọ ni isalẹ lati pari Dropbox ašẹ lati àkọọlẹ rẹ.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Iwọ yoo nilo a Dropbox iroyin to muu ti agbegbe rẹ server folda si awọsanma-orisun iṣẹ.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Bayi wipe o ni rẹ Dropbox iroyin oso, tẹ lori "Mu awọn bayi" Bọtini. O yoo wa ni pese pẹlu kan URL ni awọn a kiri lati awọn Dropbox folda gbé ni ile rẹ liana. Lo yi ọna asopọ lati láti rẹ Dropbox iroyin pẹlu yi nlo nipa tite lori o ati ipari awọn Dropbox ašẹ.';
+$lang['user_dropbox_invalid_account'] = 'Account ni invalid.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Binu, awọn Dropbox iṣẹ ni ko wa fun awọn root iroyin.';
+$lang['user_dropbox_starting_account_init'] = 'Ti o bere iroyin initialization ... jọwọ jẹ alaisan.';
+$lang['user_dropbox_step_1'] = 'Igbese 1 of 2';
+$lang['user_dropbox_step_2'] = 'Igbese 2 of 2';
+$lang['user_dropbox_sync_check'] = 'ṣayẹwo Sync';
+$lang['user_dropbox_sync_now'] = 'muu Bayi';
+$lang['user_dropbox_url_link'] = 'Dropbox URL asopọ';
+$lang['user_dropbox_waiting_confirmation'] = 'Nduro fun Dropbox iroyin amuṣiṣẹpọ ìfàṣẹsí lati pari. Jọwọ tẹ lori Dropbox URL asopọ ni isalẹ ti o ba ti o ba ti ko ṣàbẹwò ni Dropbox ojula ati ti sopọ mọ àkọọlẹ rẹ.';
