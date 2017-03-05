3a75869baffdc51f073b54846d2371c15ee6052d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:03:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'ड्रपबक्स बादल आधारित फाइल भण्डारण र समिकरण सेवा हो। जो त त्यसै Dropbox खाता (ल्यापटप, मोबाइल, ट्याब्लेट, आदि) गर्न सम्बन्धित कुनै पनि उपकरण पहुँच गर्न सकिन्छ आफ्नो घर निर्देशिका स्थित एक फोल्डरमा फाइल समक्रमण गर्न यो अनुप्रयोग प्रयोग गर्नुहोस्।';
+$lang['user_dropbox_app_name'] = 'ड्रपबक्स';
+$lang['user_dropbox_authenticate_to_service'] = 'Dropbox संग प्रमाणित';
+$lang['user_dropbox_authentication'] = 'Dropbox साथ प्रमाणित गरेर पूर्ण सेटअप';
+$lang['user_dropbox_create_account'] = 'Dropbox खाता सिर्जना गर्नुहोस्';
+$lang['user_dropbox_have_account'] = 'म पहिले देखि नै एक Dropbox खाता छ';
+$lang['user_dropbox_init_complete'] = 'प्रारम्भ पूरा भयो। तलको लिंक मा क्लिक गर्नुहोस् तपाईंको खातामा Dropbox प्राधिकरण पूरा गर्नको लागि।';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'तपाईं बादल आधारित सेवा गर्न आफ्नो स्थानीय सर्भर फोल्डर समक्रमण गर्न एक Dropbox खाता आवश्यक हुनेछ।';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'अब तपाईं आफ्नो Dropbox खाता सेटअप छ कि, "अब सिंक्रोनाइज गर्नुहोस्" बटन क्लिक गर्नुहोस्। तपाईं Dropbox फोल्डर आफ्नो घर निर्देशिका रहछन एक कुञ्जी भएको URL सँग प्रदान गरिनेछ। यसमा क्लिक गरेर र Dropbox प्राधिकरण गरेर यो गन्तव्य संग आफ्नो Dropbox खाता सम्बद्ध लागि यो लिंक प्रयोग गर्नुहोस्।';
+$lang['user_dropbox_invalid_account'] = 'खाता अमान्य छ।';
+$lang['user_dropbox_not_available_for_root_account'] = 'माफ गर्नुहोस्, Dropbox सेवा मूल खाताको लागि उपलब्ध छ।';
+$lang['user_dropbox_starting_account_init'] = 'खाता प्रारम्भ सुरु गर्दै ... कृपया धैर्य गर्नुहोस्।';
+$lang['user_dropbox_step_1'] = '2 को चरण 1';
+$lang['user_dropbox_step_2'] = '2 को चरण 2';
+$lang['user_dropbox_sync_check'] = 'सिंक जाँच';
+$lang['user_dropbox_sync_now'] = 'अब सिंक्रोनाइज गर्नुहोस्';
+$lang['user_dropbox_url_link'] = 'ड्रपबक्स यूआरएल लिंक';
+$lang['user_dropbox_waiting_confirmation'] = 'पूरा गर्न Dropbox खाता समिकरण प्रमाणीकरणको लागि प्रतीक्षा गर्दै। को Dropbox यूआरएल लिंक मा क्लिक गर्नुहोस् तपाईं Dropbox साइट भ्रमण गर्नुभएको छ भने तल छैन र तपाईंको खाता जोडिएको।';
