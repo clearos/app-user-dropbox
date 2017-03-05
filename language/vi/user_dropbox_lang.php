6d19770f927b122bb86864ac3e500aad730d3209
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:10:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox là một tập tin lưu trữ dựa trên đám mây và dịch vụ đồng bộ. Sử dụng ứng dụng này để đồng bộ hóa các tập tin vào một thư mục nằm trong thư mục chính của bạn mà sau đó có thể được truy cập bởi bất kỳ thiết bị liên quan đến tài khoản Dropbox cùng (máy tính xách tay, điện thoại di động, tablet, vv).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Xác thực với Dropbox';
+$lang['user_dropbox_authentication'] = 'Toàn bộ thiết lập bằng chứng thực với Dropbox';
+$lang['user_dropbox_create_account'] = 'Tạo tài khoản Dropbox';
+$lang['user_dropbox_have_account'] = 'tôi đã có một tài khoản Dropbox';
+$lang['user_dropbox_init_complete'] = 'Khởi tạo hoàn chỉnh. Vui lòng click vào link dưới đây để hoàn thành uỷ quyền Dropbox đến tài khoản của bạn.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Bạn sẽ cần một tài khoản Dropbox để đồng bộ hóa thư mục máy chủ của bạn với các dịch vụ dựa trên đám mây.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Bây giờ bạn đã thiết lập tài khoản Dropbox của bạn, nhấp vào nút "Synchronize Now". Bạn sẽ được cung cấp một URL có chứa một chìa khóa để các thư mục Dropbox cư trú trong thư mục nhà của bạn. Sử dụng liên kết này để kết hợp tài khoản Dropbox của bạn với điểm đến này bằng cách nhấp vào nó và hoàn thành các phép Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Tài khoản không hợp lệ.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Xin lỗi, dịch vụ Dropbox là không có sẵn cho tài khoản gốc.';
+$lang['user_dropbox_starting_account_init'] = 'Bắt đầu khởi tạo tài khoản ... hãy kiên nhẫn.';
+$lang['user_dropbox_step_1'] = 'Bước 1 của 2';
+$lang['user_dropbox_step_2'] = 'Bước 2 của 2';
+$lang['user_dropbox_sync_check'] = 'Kiểm tra Sync';
+$lang['user_dropbox_sync_now'] = 'Đồng bộ hóa Bây giờ';
+$lang['user_dropbox_url_link'] = 'Liên kết Dropbox URL';
+$lang['user_dropbox_waiting_confirmation'] = 'Đang chờ Dropbox xác thực đồng bộ hóa tài khoản để hoàn thành. Vui lòng click vào các liên kết URL Dropbox dưới đây nếu bạn đã không truy cập trang web Dropbox và liên kết tài khoản của bạn.';
