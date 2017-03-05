0190d241d8fe83ca8dc41b8ffe24944f0ba16ffd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:06:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox เป็นจัดเก็บไฟล์เมฆตามและบริการประสาน ใช้ app นี้เพื่อประสานไฟล์ไปยังโฟลเดอร์ที่ตั้งอยู่ในไดเรกทอรีบ้านของคุณที่สามารถเข้าถึงได้โดยอุปกรณ์ใด ๆ ที่เกี่ยวข้องกับบัญชี Dropbox เดียวกัน (แล็ปท็อป, โทรศัพท์มือถือ, แท็บเล็ตอื่น ๆ )';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'ตรวจสอบกับ Dropbox';
+$lang['user_dropbox_authentication'] = 'ติดตั้งเสร็จสมบูรณ์โดยการตรวจสอบสิทธิ์กับ Dropbox';
+$lang['user_dropbox_create_account'] = 'สร้างบัญชี Dropbox';
+$lang['user_dropbox_have_account'] = 'ฉันมีบัญชี Dropbox แล้ว';
+$lang['user_dropbox_init_complete'] = 'เริ่มต้นที่สมบูรณ์ กรุณาคลิกที่ลิงค์ด้านล่างเพื่อดำเนินการให้สิทธิ์ Dropbox ในบัญชีของคุณ';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'คุณจะต้องมีบัญชี Dropbox เพื่อประสานโฟลเดอร์เซิร์ฟเวอร์ในท้องถิ่นของคุณไปยังบริการ Cloud-Based';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'ตอนนี้คุณมีการตั้งค่าบัญชี Dropbox ของคุณให้คลิกที่ปุ่ม "ประสานตอนนี้" คุณจะได้รับที่มี URL ที่มีกุญแจสำคัญในการโฟลเดอร์ Dropbox พำนักอยู่ในไดเรกทอรีบ้านของคุณ ใช้ลิงค์นี้เพื่อเชื่อมโยงบัญชี Dropbox ของคุณกับปลายทางนี้โดยคลิกที่มันและเสร็จสิ้นการอนุมัติ Dropbox';
+$lang['user_dropbox_invalid_account'] = 'บัญชีไม่ถูกต้อง';
+$lang['user_dropbox_not_available_for_root_account'] = 'ขออภัยบริการ Dropbox ไม่สามารถใช้ได้สำหรับบัญชีราก';
+$lang['user_dropbox_starting_account_init'] = 'เริ่มต้นการเริ่มต้นบัญชี ... โปรดรอสักครู่';
+$lang['user_dropbox_step_1'] = 'ขั้นตอนที่ 1 จาก 2';
+$lang['user_dropbox_step_2'] = 'ขั้นตอนที่ 2 จาก 2';
+$lang['user_dropbox_sync_check'] = 'ตรวจสอบการซิงค์';
+$lang['user_dropbox_sync_now'] = 'ประสานตอนนี้';
+$lang['user_dropbox_url_link'] = 'URL ของลิงก์ Dropbox';
+$lang['user_dropbox_waiting_confirmation'] = 'รอสำหรับการตรวจสอบการประสานบัญชี Dropbox ให้เสร็จสมบูรณ์ โปรดคลิกที่ URL ของลิงก์ Dropbox ด้านล่างหากคุณยังไม่ได้เข้าเยี่ยมชมเว็บไซต์ Dropbox และเชื่อมโยงบัญชีของคุณ';
