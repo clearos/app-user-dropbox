bf3b8754482820e9b842e022612930e87f0aa605
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:03:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox er en sky-basert fillagring og synkroniseringstjeneste. Bruk denne applikasjonen til å synkronisere filer til en mappe som ligger på hjemmeområdet som deretter kan nås av alle enheter knyttet til samme Dropbox-kontoen (laptop, mobil, nettbrett osv).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Verifisere med Dropbox';
+$lang['user_dropbox_authentication'] = 'Komplett oppsett ved å autentisere med Dropbox';
+$lang['user_dropbox_create_account'] = 'Opprett Dropbox-konto';
+$lang['user_dropbox_have_account'] = 'Jeg har allerede en Dropbox-konto';
+$lang['user_dropbox_init_complete'] = 'Initialisering komplett. Vennligst klikk på linken nedenfor for å fullføre Dropbox fullmakt til kontoen din.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Du trenger en Dropbox-konto for å synkronisere lokale serveren mappen til cloud-basert tjeneste.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Nå som du har din Dropbox-konto oppsett, klikker du på "Synkroniser nå" -knappen. Du vil bli utstyrt med en URL som inneholder en nøkkel til Dropbox-mappen bosatt i hjemmekatalogen. Bruk denne linken for å knytte Dropbox-konto med dette målet ved å klikke på den og fullføre Dropbox autorisasjon.';
+$lang['user_dropbox_invalid_account'] = 'Konto er ugyldig.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Beklager, Dropbox-tjenesten ikke er tilgjengelig for root-kontoen.';
+$lang['user_dropbox_starting_account_init'] = 'Starter konto initialisering ... vær tålmodig.';
+$lang['user_dropbox_step_1'] = 'Trinn 1 av 2';
+$lang['user_dropbox_step_2'] = 'Trinn 2 av 2';
+$lang['user_dropbox_sync_check'] = 'Sjekk Sync';
+$lang['user_dropbox_sync_now'] = 'synkron~~POS=TRUNC nå';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Venter på Dropbox-konto synkronisering godkjenning for å fullføre. Vennligst klikk på Dropbox URL-lenke nedenfor hvis du ikke har besøkt Dropbox nettstedet og tilknyttet kontoen din.';
