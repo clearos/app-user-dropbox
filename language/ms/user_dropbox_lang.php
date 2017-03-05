833035610de675386c7826659bc14829bfa6e060
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:02:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox adalah penyimpanan fail berasaskan awan dan perkhidmatan penyegerakan. Menggunakan aplikasi ini untuk menyegerakkan fail ke folder terletak dalam direktori rumah yang kemudiannya boleh diakses oleh mana-mana peranti yang dikaitkan ke akaun Dropbox yang sama (komputer riba, telefon bimbit, tablet dan lain-lain).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Mengesahkan dengan Dropbox';
+$lang['user_dropbox_authentication'] = 'Lengkapkan persediaan dengan mengesahkan dengan Dropbox';
+$lang['user_dropbox_create_account'] = 'Buat akaun Dropbox';
+$lang['user_dropbox_have_account'] = 'Saya sudah mempunyai akaun Dropbox';
+$lang['user_dropbox_init_complete'] = 'Pengawalan yang lengkap. Sila klik pada pautan di bawah untuk melengkapkan Dropbox kebenaran ke akaun anda.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Anda akan memerlukan akaun Dropbox untuk menyegerakkan folder pelayan tempatan anda kepada perkhidmatan berasaskan awan.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Sekarang bahawa anda mempunyai persediaan akaun Dropbox anda, klik pada butang "Segerakkan sekarang". Anda akan disediakan dengan URL yang mengandungi kunci untuk folder Dropbox yang tinggal di dalam direktori rumah anda. Gunakan pautan ini untuk mengaitkan akaun Dropbox anda dengan destinasi ini dengan mengklik padanya dan melengkapkan kebenaran Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Akaun tidak sah.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Maaf, perkhidmatan Dropbox tidak tersedia untuk akaun akar.';
+$lang['user_dropbox_starting_account_init'] = 'Bermula akaun pengawalan ... sila bersabar.';
+$lang['user_dropbox_step_1'] = 'Langkah 1 dari 2';
+$lang['user_dropbox_step_2'] = 'Langkah 2 dari 2';
+$lang['user_dropbox_sync_check'] = 'Semak Sync';
+$lang['user_dropbox_sync_now'] = 'Segerakkan sekarang';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Menunggu Dropbox pengesahan penyegerakan akaun untuk disiapkan. Sila klik pada Dropbox URL Link di bawah jika anda tidak melawat laman web Dropbox dan dikaitkan akaun anda.';
