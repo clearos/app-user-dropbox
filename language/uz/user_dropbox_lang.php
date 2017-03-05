2f250dae32f993890d689e1a81910eca62f42cb1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:06:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox bulut asoslangan fayl saqlash va sinxronizatsiya xizmatdir. keyin bir xil Dropbox hisob (boshqalar Noutbuklarni, mobil, planshet) uchun bog&#39;liq bo&#39;lgan qurilma orqali erishish mumkin, siz uy joylashgan katalogda joylashgan papkaga fayllar sinxronlashtirish uchun ushbu ilovadan foydalaning.';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Dropbox bilan aniqlashda';
+$lang['user_dropbox_authentication'] = 'Dropbox bilan autentifikatsiya tomonidan to&#39;liq o&#39;rnatish';
+$lang['user_dropbox_create_account'] = 'Dropbox Hisob yaratish';
+$lang['user_dropbox_have_account'] = 'Men allaqachon Dropbox hisobi bor';
+$lang['user_dropbox_init_complete'] = 'Boshlash to&#39;liq. hisobingizga Dropbox ruxsat bajarish uchun quyidagi link bosing.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Siz bulutga asoslangan xizmatiga mahalliy server papkani sinxronlashtirish uchun Dropbox hisobini kerak bo&#39;ladi.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Endi siz Dropbox hisob o&#39;rnatish bor, deb, "Endi hamohang" tugmasini bosing. Agar uy katalogiga yashovchi Dropbox papkaga bir kalit o&#39;z ichiga olgan URL bilan taqdim etiladi. u bosgan va Dropbox ruxsat yakunlab, bu joyga bilan Dropbox hisobini sherik bu linkni foydalaning.';
+$lang['user_dropbox_invalid_account'] = 'Hisobi yaroqsiz.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Kechirasiz, Dropbox xizmat ildiz hisob uchun foydalanish mumkin emas.';
+$lang['user_dropbox_starting_account_init'] = 'hisob boshlash boshlash ... iltimos, sabr qiling.';
+$lang['user_dropbox_step_1'] = '2-bosqich: 1';
+$lang['user_dropbox_step_2'] = '2-bosqich 2';
+$lang['user_dropbox_sync_check'] = 'Sinxronlashtirish tekshiring';
+$lang['user_dropbox_sync_now'] = 'Endi sinxronlashtirish';
+$lang['user_dropbox_url_link'] = 'Dropbox URL bog&#39;laning';
+$lang['user_dropbox_waiting_confirmation'] = 'Dropbox hisob sinxronizatsiya autentifikatsiya kutib bajarish uchun. Agar Dropbox sayt tashrif buyurdi va hisob bilan bog&#39;liq bo&#39;lsa, quyida Dropbox URL Link bosing.';
