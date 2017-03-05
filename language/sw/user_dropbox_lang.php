e036b70a9802ce5f010963a90d084bff6065ae04
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:05:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox ni wingu makao faili kuhifadhi na huduma ya uoanishaji. Kutumia programu hii kwa synchronize files kwa folder iko katika nyumba saraka yako ambayo yanaweza kupatikana kwa kifaa chochote kuhusishwa na sawa Dropbox akaunti (laptop, simu, kibao nk).';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Kuthibitisha na Dropbox';
+$lang['user_dropbox_authentication'] = 'Kamili kuanzisha na kuthibitisha na Dropbox';
+$lang['user_dropbox_create_account'] = 'Kuunda akaunti Dropbox';
+$lang['user_dropbox_have_account'] = 'Mimi tayari kuwa na akaunti Dropbox';
+$lang['user_dropbox_init_complete'] = 'Initialization kamili. Tafadhali bonyeza kiungo chini ili kukamilisha Dropbox idhini kwa akaunti yako.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Unahitaji akaunti Dropbox synchronize kompyuta yako folder mitaa na huduma wingu makao.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Sasa kwa kuwa una akaunti Dropbox yako kuanzisha, bonyeza "Synchronize Sasa" kifungo. Utakuwa zinazotolewa na URL zenye muhimu kwa folder Dropbox wanaoishi katika nyumba saraka yako. Matumizi ya kiungo huu ili kuhusisha akaunti yako Dropbox na marudio hii kwa kubonyeza juu yake na kukamilisha idhini Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Akaunti ni batili.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Samahani, huduma Dropbox haipatikani kwa akaunti mizizi.';
+$lang['user_dropbox_starting_account_init'] = 'Kuanzia akaunti initialization ... tafadhali kuwa na subira.';
+$lang['user_dropbox_step_1'] = 'Hatua ya 1 ya 2';
+$lang['user_dropbox_step_2'] = 'Hatua ya 2 ya 2';
+$lang['user_dropbox_sync_check'] = 'Angalia Sync';
+$lang['user_dropbox_sync_now'] = 'synchronize Sasa';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Kusubiri kwa Dropbox uthibitisho akaunti synchronization kukamilisha. Tafadhali bonyeza Dropbox URL Link chini kama wewe si alitembelea tovuti Dropbox na wanaohusishwa akaunti yako.';
