874939905ad37f9bc71b7a1b5612f12168778886
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:02:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox е облак-базирани чување на датотеки и сервис за синхронизација. Користете ја оваа апликација за да ги синхронизирате датотеки во папка се наоѓа во вашата домашна папка, кои потоа може да се пристапи од страна на секој уред поврзан со истиот Dropbox сметка (лаптоп, мобилен телефон, таблет и др.)';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Идентификувате со Dropbox';
+$lang['user_dropbox_authentication'] = 'Целосно поставување од страна на автентикација со Dropbox';
+$lang['user_dropbox_create_account'] = 'Креирај Dropbox сметка';
+$lang['user_dropbox_have_account'] = 'Јас веќе имате сметка на Dropbox';
+$lang['user_dropbox_init_complete'] = 'Иницијализација заврши. Ве молиме кликнете на линкот подолу за да го комплетира овластувањето Dropbox на вашиот профил.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Ќе треба Dropbox сметка за синхронизација на вашата папка локален сервер на услугата облак-базирани.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Сега дека сте го поставиле Dropbox сметка, кликнете на "Синхронизирај сега" копчето. Ќе бидат обезбедени со URL-то содржи клучот на Dropbox папка престојуваат во вашиот домашен директориум. Користете го овој линк за да го поврзете вашиот Dropbox сметка со оваа дестинација со кликнување на него и дополнување на одобрението Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Сметка е неважечка.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Жал ми е, сервис Dropbox не е достапна за root сметката.';
+$lang['user_dropbox_starting_account_init'] = 'Почетна сметка иницијализација ... Ве молиме да бидете трпеливи.';
+$lang['user_dropbox_step_1'] = 'Чекор 1 од 2';
+$lang['user_dropbox_step_2'] = 'Чекор 2 од 2';
+$lang['user_dropbox_sync_check'] = 'Проверете Sync';
+$lang['user_dropbox_sync_now'] = 'Синхронизирај сега';
+$lang['user_dropbox_url_link'] = 'Dropbox URL линк';
+$lang['user_dropbox_waiting_confirmation'] = 'Чекајќи проверка синхронизација сметка Dropbox да се заврши. Ве молиме кликнете на Dropbox URL линк подолу ако не сте го посетиле сајтот Dropbox и се поврзани на вашата сметка.';
