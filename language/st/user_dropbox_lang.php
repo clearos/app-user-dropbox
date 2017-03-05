415c0fd8853ca9045834995c871bb37d96eb561d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:05:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'Dropbox ke leru thehiloeng faele polokelo le tšebeletso kamahanyo. Sebelisa app e sena ho nyalanya difaele foldareng teng lapeng dibaka ya hao e leng ka ka nako eo ho accessed ke sesebediswa leha e le efe e amanang ho tšoanang Dropbox akhaonte (viettel, mobile, letlapa jj.)';
+$lang['user_dropbox_app_name'] = 'Dropbox';
+$lang['user_dropbox_authenticate_to_service'] = 'Ho hlapantsa le Dropbox';
+$lang['user_dropbox_authentication'] = 'Complete seta hammamorao ho ke authenticating le Dropbox';
+$lang['user_dropbox_create_account'] = 'Create akhaonte Dropbox';
+$lang['user_dropbox_have_account'] = 'Ke se a ntse a e-na le akhaonto ya Dropbox';
+$lang['user_dropbox_init_complete'] = 'Initialization feletseng. Ka kōpo tobetsa kgokahanyo mona ka tlase ho tlatsa Dropbox thebolo tlalehong ea lōna.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'O tla hloka tlaleho Dropbox ho nyalanya seva ya hao foldareng ya selehae ya tshebeletso leru tse thehiloeng Bibeleng.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Kaha joale u na le akhaonto ya Dropbox oa hao seta hammamorao ho, tobetsa ka "nyalanya hona joale" konopo. O tla fuwa nang le URL nang le senotlolo sa ho foldareng ya Dropbox lulang lehaeng la dibaka tsa hao. Sebelisa link tsa sena ho tloaelana akhaonteng ya hao Dropbox le eang teng ketsahalong ena ka ho tobetsa ka eona &#39;me ho phethang thebolo Dropbox.';
+$lang['user_dropbox_invalid_account'] = 'Account ha o sebetse.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Sorry, tšebeletso Dropbox ha fumaneha bakeng sa ho tlaleho ea mela.';
+$lang['user_dropbox_starting_account_init'] = 'Ho qala tlaleho ena e initialization ... ka kōpo ba le mamello.';
+$lang['user_dropbox_step_1'] = 'Mohato oa 1 of 2';
+$lang['user_dropbox_step_2'] = 'Mohato oa 2 of 2';
+$lang['user_dropbox_sync_check'] = 'Check kgokagantswe';
+$lang['user_dropbox_sync_now'] = 'nyalanya Joale';
+$lang['user_dropbox_url_link'] = 'Dropbox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Emetse Dropbox netefatso akhaonte kamahanyo ho tlatsa. Ka kōpo tobetsa ea Dropbox URL Link mona ka tlase ha u sa etela sebakeng sena Dropbox le amahanngoa akhaonteng ya hao.';
