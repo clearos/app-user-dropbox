553d522dca4760e5c9ca535c272765e9d1f99ae5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 00:04:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/user_dropbox_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['user_dropbox_app_description'] = 'DropBox waa kaydinta file daruur ku salaysan iyo adeegga .Wadashaqayntaas. Isticmaal app this waafajinta files in folder ah oo ku yaalla buugga gurigaaga taas oo ka dibna waxaa laga heli karaa by qalab kasta oo la xiriira in ay ku account DropBox isku mid ah (laptop, mobile, kiniin iwm).';
+$lang['user_dropbox_app_name'] = 'DropBox';
+$lang['user_dropbox_authenticate_to_service'] = 'Sugee la DropBox';
+$lang['user_dropbox_authentication'] = 'Mudanayaasha Buuxi by authenticating la DropBox';
+$lang['user_dropbox_create_account'] = 'Create account DropBox';
+$lang['user_dropbox_have_account'] = 'Waxaan hore u leedahay xisaabta DropBox a';
+$lang['user_dropbox_init_complete'] = 'Initialization dhamaystiran. Fadlan guji link hoose si aad u buuxiso oggolaansho DropBox in aad xisaabta.';
+$lang['user_dropbox_initialize_dropbox_setup_help'] = 'Waxaad u baahan doontaa xisaab DropBox ah waafajinta aad folder server deegaanka si ay adeeg daruur ku salaysan ka.';
+$lang['user_dropbox_initialize_dropbox_sync_help'] = 'Hadda aad leedahay si aad ah Mudanayaasha xisaabta DropBox, riix "waafajinta Hadda" button. Waxaad la siin doonaa URL ka kooban fure u folder DropBox deggan buugga gurigaaga. Isticmaal link this la wadaajin account DropBox aad la socoto this adigoo gujinaya ku yaal oo buuxinaya oggolaansho DropBox ah.';
+$lang['user_dropbox_invalid_account'] = 'Account waa baadil.';
+$lang['user_dropbox_not_available_for_root_account'] = 'Sorry, adeeg DropBox ma loo heli karaa account xididka.';
+$lang['user_dropbox_starting_account_init'] = 'Laga bilaabo initialization account ... fadlan samir.';
+$lang['user_dropbox_step_1'] = 'Tallaabada 1 of 2';
+$lang['user_dropbox_step_2'] = 'Tallaabada 2 of 2';
+$lang['user_dropbox_sync_check'] = 'Hubi nidaameed';
+$lang['user_dropbox_sync_now'] = 'waafajinta Hadda';
+$lang['user_dropbox_url_link'] = 'DropBox URL Link';
+$lang['user_dropbox_waiting_confirmation'] = 'Sugitaanka DropBox sugida account .Wadashaqayntaas si uu u dhameystiro. Fadlan guji URL DropBox ku Link hoose haddii aadan booqday goobta DropBox iyo lala xisaabtaada.';
